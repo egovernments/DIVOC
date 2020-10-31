86e2f8f9d701c98a16812695e110742ee2e41f99
Burhanuddin Rashid <burhanuddinr@sahajsoft.com>
2020-10-31 08:16:50 +0530

    [#6] New Enroll Dob and nationality added.

--- mobile/lib/forms/new_user_form.dart
+++ mobile/lib/forms/new_user_form.dart
@@ -1,13 +1,27 @@
 import 'package:divoc/base/common_widget.dart';
 import 'package:divoc/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'navigation_flow.dart';
 
-class NewUserEnrollForm extends StatelessWidget {
+class NewUserEnrollForm extends StatefulWidget {
   final RouteInfo routeInfo;
 
   NewUserEnrollForm(this.routeInfo);
+
+  @override
+  _NewUserEnrollFormState createState() => _NewUserEnrollFormState();
+}
+
+class _NewUserEnrollFormState extends State<NewUserEnrollForm> {
+  final textController = new TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +40,7 @@ class NewUserEnrollForm extends StatelessWidget {
                   return msg;
                 },
                 decoration: InputDecoration(
-                  labelText: "Name :",
+                  labelText: "Name",
                   border: OutlineInputBorder(),
                 ),
               ),
@@ -41,7 +55,7 @@ class NewUserEnrollForm extends StatelessWidget {
                   return msg;
                 },
                 decoration: InputDecoration(
-                  labelText: "Email :",
+                  labelText: "Email",
                   border: OutlineInputBorder(),
                 ),
               ),
@@ -56,18 +70,63 @@ class NewUserEnrollForm extends StatelessWidget {
                   return msg;
                 },
                 decoration: InputDecoration(
-                  labelText: "Mobile :",
+                  labelText: "Mobile",
+                  prefixText: "+91 ",
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                child: Text("DONE"),
+              child: TextFormField(
+                readOnly: true,
+                controller: textController,
+                showCursor: true,
+                keyboardType: TextInputType.datetime,
+                onSaved: (value) {},
+                validator: (value) {
+                  var msg = value.isEmpty ? "Cannot be Empty" : null;
+                  return msg;
+                },
+                decoration: InputDecoration(
+                  labelText: "DOB",
+                  prefixIcon: IconButton(
+                    icon: Icon(Icons.date_range),
+                    onPressed: () async {
+                      final selectTime = await _selectTime(context);
+                      if (selectTime != null) {
+                        String formattedDate =
+                            DateFormat('dd-MM-yyyy').format(selectTime);
+                        textController.text = formattedDate;
+                      }
+                    },
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.datetime,
+                onSaved: (value) {},
+                validator: (value) {
+                  var msg = value.isEmpty ? "Cannot be Empty" : null;
+                  return msg;
+                },
+                decoration: InputDecoration(
+                  labelText: "Nationality",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FormButton(
+                text: "DONE",
                 onPressed: () {
-                  NavigationFormFlow.push(
-                      context, routeInfo.nextRoutesMeta[0].fullNextRoutePath);
+                  NavigationFormFlow.push(context,
+                      widget.routeInfo.nextRoutesMeta[0].fullNextRoutePath);
                 },
               ),
             )
@@ -75,5 +134,34 @@ class NewUserEnrollForm extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<DateTime> _selectTime(BuildContext context) async {
+    /*TimeOfDay selectedTime = TimeOfDay.now();
+    final TimeOfDay datePicked = await showTimePicker(
+        context: context,
+        initialTime: selectedTime,
+        builder: (BuildContext context, Widget child) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: false),
+            child: child,
+          );
+        });
+
+    return datePicked;*/
+
+    final datePicker = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        //which date will display when user open the picker
+        firstDate: DateTime(1950),
+        //what will be the previous supported year in picker
+        lastDate: DateTime.now());
+    return datePicker;
+
+    /* if (datePicked != null && datePicked != selectedTime )
+      setState(() {
+        selectedTime = datePicked;
+      });*/
   }
 }
