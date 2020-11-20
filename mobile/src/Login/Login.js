508ff80505ad0439b8a0ded2938ce58bb13fa9ce
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2020-11-20 12:19:07 +0530

    Fixed height issue

--- mobile/src/Login/Login.js
+++ mobile/src/Login/Login.js
@@ -5,18 +5,21 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {useHistory} from "react-router";
 import {VerifyOTPComponent} from "./VerifyOTPComponent";
 import {EnterPhoneNumberComponent} from "./EnterPhoneNumberComponent";
+import Card from "react-bootstrap/Card";
 
 export function LoginComponent() {
     return <div className={"login-container"}>
-        <AppLogo/>
-        <LoginProvider>
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={EnterPhoneNumberComponent}/>
-                    <Route path="/otp" exact component={VerifyOTPComponent}/>
-                </Switch>
-            </Router>
-        </LoginProvider>
+        <Card>
+            <AppLogo/>
+            <LoginProvider>
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={EnterPhoneNumberComponent}/>
+                        <Route path="/otp" exact component={VerifyOTPComponent}/>
+                    </Switch>
+                </Router>
+            </LoginProvider>
+        </Card>
     </div>
 }
 
