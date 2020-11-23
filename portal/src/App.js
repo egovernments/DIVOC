d5a70cb46c0a33d1b2313a448065a65e5794453e
Tejash <tejash.jl1020@gmail.com>
2020-11-23 15:25:20 +0530

    - added keycloak client library  - created PrivateRoute to show login page if not authorized

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,48 +1,49 @@
 import './App.css';
 import logo from './logo.svg';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import Home from './components/Home'
 import About from './components/About'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
+import {PrivateRoute} from "./utils/PrivateRoute";
+import {useKeycloak} from "@react-keycloak/web";
+import React from "react";
 
 export default function App() {
-    return (
+  const {initialized, keycloak} = useKeycloak();
+  if (!initialized) {
+    return <div>Loading...</div>
+  }
+  return (
+    <div>
+      <div><img src={logo}></img></div>
+      <Router>
         <div>
-          <div><img src={logo}></img></div>
-            <Router>
-                <div>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/dashboard">Dashboard</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/about">What is DIVOC?</Link></li>
-                    </ul>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/about">What is DIVOC?</Link></li>
+            <li><button onClick={() => {keycloak.logout()}}>LOGOUT</button></li>
+          </ul>
 
-                    <hr/>
+          <hr/>
 
-                    {/*
+          {/*
           A <Switch> looks through all its children <Route>
           elements and renders the first one whose path
           matches the current URL. Use a <Switch> any time
           you have multiple routes, but you want only one
           of them to render at a time
         */}
-                    <Switch>
-                        <Route exact path="/">
-                            <Home/>
-                        </Route>
-                        <Route path="/login"><Login/></Route>
-                        <Route path="/dashboard"><Dashboard/></Route>
-                        <Route path="/about"><About/></Route>
-                    </Switch>
-                </div>
-            </Router>
+          <Switch>
+            <PrivateRoute exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login}/>
+            <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            <PrivateRoute exact path="/about" component={About}/>
+          </Switch>
         </div>
-    );
+      </Router>
+    </div>
+  );
 }
