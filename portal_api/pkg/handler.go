3f3fdcc66aa165572ca713a0d92cccaccf2b35ea
Dileep Bapat <dileepbapat@gmail.com>
2020-11-24 15:59:12 +0530

    Adding create medice relay request to registry

--- portal_api/pkg/handler.go
+++ portal_api/pkg/handler.go
@@ -1,7 +1,13 @@
 package pkg
 
-import ("github.com/divoc/portal-api/swagger_gen/restapi/operations"
+import (
+	"encoding/json"
+	"github.com/divoc/portal-api/config"
+	"github.com/divoc/portal-api/swagger_gen/restapi/operations"
+	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
+	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func SetupHandlers(api *operations.DivocPortalAPIAPI) {
@@ -11,8 +17,46 @@ func SetupHandlers(api *operations.DivocPortalAPIAPI) {
 	api.PostVaccinatorsHandler = operations.PostVaccinatorsHandlerFunc(postVaccinatorsHandler)
 }
 
+type GenericRequestError struct {
+	statusCode int
+}
+
+func (o *GenericRequestError) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+
+	rw.Header().Del(runtime.HeaderContentType) //Remove Content-Type on empty responses
+
+	rw.WriteHeader(o.statusCode)
+}
+
+func NewGenericStatusOk() middleware.Responder {
+	return &GenericRequestError{statusCode:200}
+}
+
+func NewGenericServerError() middleware.Responder {
+	return &GenericRequestError{statusCode:200}
+}
+
+
 func createMedicineHandler(params operations.CreateMedicineParams, principal interface{}) middleware.Responder {
-	return operations.NewCreateMedicineOK()
+	log.Infof("Create medicine %+v", params.Body)
+	objectId:="Medicine"
+	requestBody, err := json.Marshal(params.Body)
+	if err != nil {
+		return operations.NewCreateMedicineBadRequest()
+	}
+	requestMap := make(map[string]interface{})
+	err = json.Unmarshal(requestBody, &requestMap)
+	if err != nil {
+		log.Info(err)
+		return &GenericRequestError{statusCode:500}
+	}
+	return makeRegistryCreateRequest(requestMap, objectId)
+}
+
+
+func registryUrl(operationId string) string {
+	url := config.Config.Registry.Url + "/" + operationId
+	return url
 }
 func createProgramHandler(params operations.CreateProgramParams, principal interface{}) middleware.Responder {
 	return operations.NewCreateProgramOK()
