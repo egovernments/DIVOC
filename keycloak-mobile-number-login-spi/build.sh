58abc4b3a54724fed6006037e66b7b554d5128a7
Dileep Bapat <dileepbapat@gmail.com>
2020-11-26 13:22:54 +0530

    Key cloak theme and provider bundling to image

--- keycloak-mobile-number-login-spi/build.sh
+++ keycloak-mobile-number-login-spi/build.sh
@@ -1,4 +1,4 @@
-./mvnw clean install
-mkdir -p ../../keycloak-11.0.3/providers && cp target/keycloak-mobile-number-login-spi-1.0-SNAPSHOT.jar ../../keycloak-11.0.3/providers
-cp -r themes/sys-admin ../../keycloak-11.0.3/themes/
-cp -r themes/facility-operator ../../keycloak-11.0.3/themes/
+#./mvnw clean install
+mkdir -p ../keycloak/providers && cp target/keycloak-mobile-number-login-spi-1.0-SNAPSHOT.jar ../keycloak/providers
+cp -r themes/sys-admin ../keycloak/themes/
+cp -r themes/facility-operator ../keycloak/themes/
