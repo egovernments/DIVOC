39458f6a2d4cf4ae662dd7e8e21c9f1f31cccfc9
juhij <juhij@sahajsoft.com>
2020-11-26 11:51:10 +0530

    Changed HTML Form to react-jsonschema-form

--- portal/src/components/ProgramRegistration/ProgramRegistration.js
+++ portal/src/components/ProgramRegistration/ProgramRegistration.js
@@ -1,16 +1,107 @@
-import React from 'react';
-import ProgramRegistrationForm from '../ProgramRegistrationForm/ProgramRegistrationForm';
-import styles from './ProgramRegistration.module.css';
+import React, { useState } from "react";
+import styles from "./ProgramRegistration.module.css";
+import axios from "axios";
+import { useKeycloak } from "@react-keycloak/web";
+import Form from "@rjsf/core";
 
 function VaccineRegistration() {
-    return(
-        <div className={styles['container']}>
-            <div className={styles['registration-form']}>
-            <ProgramRegistrationForm />
-        </div>
-        <div className={styles['registration-form']}>
-            <p>List of Registered Program</p>
-        </div>
+    const { keycloak } = useKeycloak();
+    const [formData, setFormData] = useState(null);
+
+    const schema = {
+        title: "Program",
+        type: "object",
+        required: ["name", "description", "startDate"],
+        properties: {
+            name: {
+                type: "string",
+                title: "Name",
+            },
+            description: {
+                type: "string",
+                title: "Description",
+            },
+            logoURL: {
+                type: "string",
+                title: "Program Logo",
+                format: "file",
+            },
+            startDate: {
+                type: "string",
+                title: "Start Date",
+                format: "date",
+            },
+            endDate: {
+                type: "string",
+                title: "End Date",
+                format: "date",
+            },
+            status: {
+                type: "string",
+                title: "Status",
+                format: "radio",
+                enum: ["Active", "Inactive"],
+            },
+            medicineIds: {
+                type: "array",
+                items: {
+                    type: "string",
+                },
+            },
+        },
+    };
+
+    
+
+    const uiSchema = {
+        "ui:order": [
+            "name",
+            "description",
+            "logoURL",
+            "startDate",
+            "endDate",
+            "status",
+            "medicineIds",
+        ],
+        name: {
+            classNames: styles["custom-class-name"],
+        },
+        age: {
+            classNames: "custom-class-age",
+        },
+        medicineIds: { "ui:widget": "hidden" },
+    };
+
+    const handleSubmit = () => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${keycloak.token} `,
+                "Content-Type": "application/json",
+            },
+        };
+        axios
+            .post("/divoc/admin/api/v1/programs", formData, config)
+            .then((res) => {
+                alert("Status code is", res);
+                console.log(res);
+            });
+    };
+
+    return (
+        <div className={styles["container"]}>
+            <div className={styles["registration-form"]}>
+                <Form
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    onSubmit={(e) => {
+                        // setFormData(e.formData)
+                        handleSubmit(e.formData);
+                    }}
+                />
+            </div>
+            <div className={styles["registration-form"]}>
+                <p>List of Registered Program</p>
+            </div>
         </div>
     );
 }
--- portal/src/components/VaccineRegistration/VaccineRegistration.js
+++ portal/src/components/VaccineRegistration/VaccineRegistration.js
@@ -1,31 +1,96 @@
-import React from 'react';
+import React, { useState } from "react";
+import styles from "./VaccineRegistration.module.css";
+import { useKeycloak } from "@react-keycloak/web";
 import axios from "axios";
-import VaccineRegistrationForm from '../VaccineRegistrationForm/VaccineRegistrationForm';
-import styles from './VaccineRegistration.module.css';
-import {useKeycloak} from "@react-keycloak/web";
+import Form from "@rjsf/core";
 
 function VaccineRegistration() {
     const { keycloak } = useKeycloak();
+    const [formData, setFormData] = useState(null);
 
-    // const GetRequest = () => {
-    //     const config = {
-    //         headers: { "Authorization": `Bearer ${keycloak.token} `, "Content-Type": "application/json" }
-    //     };
-    //     axios
-    //         .get("/divoc/admin/api/v1/medicine", config)
-    //         .then((res) => {
-    //             alert("Status code is",res.status)
-    //             console.log(res);
-    //         });
-    // }
-    return(
-        <div className={styles['container']}>
-            <div className={styles['registration-form']}>
-            <VaccineRegistrationForm />
-        </div>
-        <div className={styles['registration-form']}>
-            {/* <GetRequest/> */}
-        </div>
+    const schema = {
+        title: "Register New Vaccine",
+        type: "object",
+        properties: {
+            name: {
+                type: "string",
+                title: "Name",
+            },
+            provider: {
+                title: "Provider",
+                type: "string",
+            },
+            schedule: {
+                title: "Schedule",
+                type: "object",
+                description:
+                    "How many times and how often vaccination should be repeated",
+                properties: {
+                    repeatTimes: {
+                        type: "number",
+                        title: "Repeat times",
+                        description:
+                            "How many times vaccination should be taken",
+                    },
+                    repeatInterval: {
+                        title: "Repeat interval",
+                        type: "number",
+                        description:
+                            "Number of times the vaccination should be taken.",
+                    },
+                },
+            },
+            effectiveUntil: {
+                type: "number",
+                description:
+                    "Effective until n months after the full vaccination schedule is completed",
+                title: "Effective until (months)",
+            },
+            status: {
+                type: "string",
+                enum: ["Active", "Inactive", "Blocked"],
+                title: "Status",
+            },
+            price: {
+                title: "Price (if fixed)",
+                type: "number",
+            },
+        },
+    };
+
+    const handleSubmit = (dataToSend) => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${keycloak.token} `,
+                "Content-Type": "application/json",
+            },
+        };
+        axios
+            .post("/divoc/admin/api/v1/medicines", dataToSend, config)
+            .then((res) => {
+                alert("Status code is", res);
+                console.log(res);
+            });
+    };
+
+    const uiSchema = {};
+
+
+    return (
+        <div className={styles["container"]}>
+            <div className={styles["registration-form"]}>
+                <Form
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    onSubmit={(e) => {
+                        // setFormData(e.formData);
+                        handleSubmit(e.formData);
+                    }}
+                />
+            </div>
+            <div className={styles["registration-form"]}>
+                <p>List of Registered Vaccines</p>
+            </div>
         </div>
     );
 }
