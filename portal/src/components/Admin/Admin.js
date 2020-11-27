0456d6853175fb883e569626707412bc74eb557a
juhij <juhij@sahajsoft.com>
2020-11-27 14:40:35 +0530

    Adding styling to Admin navigation tabs

--- portal/src/components/Admin/Admin.js
+++ portal/src/components/Admin/Admin.js
@@ -2,12 +2,49 @@ import React from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
-import Paper from '@material-ui/core/Paper';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import VaccineRegistration from "../VaccineRegistration/VaccineRegistration";
 import FacilitiesRegistry from "../FacilitiesRegistry/FacilitiesRegistry";
 import ProgramRegistration from "../ProgramRegistration/ProgramRegistration";
 import VaccinatorsRegistry from "../VaccinatorsRegistry/VaccinatorsRegistry";
 import PreEnrollment from "../PreEnrollment/PreEnrollment";
+
+
+const StyledTabs = withStyles({
+    indicator: {
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: 'transparent',
+      color: '#040E28',
+      '& > span': {
+        maxWidth: 60,
+        width: '100%',
+        backgroundColor: '#5C9EF8',
+      },
+    },
+  })((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+  const StyledTab = withStyles((theme) => ({
+    root: {
+      textTransform: 'none',
+      textAlign: 'left',
+      fontWeight: 'bold',
+      fontSize: theme.typography.pxToRem(16),
+      marginRight: theme.spacing(1),
+      '&:focus': {
+        opacity: 1,
+      },
+    },
+  }))((props) => <Tab disableRipple {...props} />);
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      paddingLeft: theme.spacing(10),
+      paddingRight: theme.spacing(10),
+      paddingTop: theme.spacing(3),
+    },
+  }));
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -18,26 +55,24 @@ function TabPanel(props) {
 
 export default function Admin() {
     const [value, setValue] = React.useState(0);
+    const classes = useStyles();
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
     return (
-        <>
-             <Paper square>
-                <Tabs
-                    value={value}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    onChange={handleChange}
-                    aria-label="disabled tabs example"
-                >
-                    <Tab label="Facilities" />
-                    <Tab label="Vaccinators" />
-                    <Tab label="Vaccine Programs" />
-                    <Tab label="Vaccines" />
-                    <Tab label="Enrollment" />
-                </Tabs>
-            </Paper>
+        <div className={classes.root}>
+            <StyledTabs
+                value={value}
+                indicatorColor="secondary"
+                onChange={handleChange}
+                aria-label="styled tabs example"
+            >
+                <StyledTab label="Facilities" />
+                <StyledTab label="Vaccinators" />
+                <StyledTab label="Vaccine Programs" />
+                <StyledTab label="Vaccines" />
+                <StyledTab label="Enrollment" />
+            </StyledTabs>
             <TabPanel value={value} index={0}>
                 <FacilitiesRegistry />
             </TabPanel>
@@ -53,6 +88,6 @@ export default function Admin() {
             <TabPanel value={value} index={4}>
                 <PreEnrollment />
             </TabPanel>
-        </>
+        </div>
     );
 }
