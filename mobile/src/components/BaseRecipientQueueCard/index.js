bff12513ab7d2178d513d5c8e99b4d506ed62e9c
Tejash <tejash.jl1020@gmail.com>
2020-11-28 09:25:46 +0530

    added initial enter batch code form

--- mobile/src/components/BaseRecipientQueueCard/index.js
+++ mobile/src/components/BaseRecipientQueueCard/index.js
@@ -4,19 +4,23 @@ import BackBtnImg from "../../assets/img/back-btn.svg";
 import React from "react";
 import "./index.scss"
 import * as PropTypes from "prop-types";
+import {useHistory} from "react-router";
 
-export const BaseRecipientQueueCard = ({title, children}) => (
-    <BaseCard>
-        <Card.Header className="d-flex justify-content-between">
-            <img src={BackBtnImg} alt={""}/>
-            <span>{title}</span>
-            <span/>
-        </Card.Header>
-        <Card.Body>
-            {children}
-        </Card.Body>
-    </BaseCard>
-);
+export const BaseRecipientQueueCard = ({title, children}) => {
+    const history = useHistory();
+    return (
+        <BaseCard>
+            <Card.Header className="d-flex justify-content-between">
+                <img src={BackBtnImg} alt={""} onClick={() => {history.goBack()}}/>
+                <span>{title}</span>
+                <span/>
+            </Card.Header>
+            <Card.Body>
+                {children}
+            </Card.Body>
+        </BaseCard>
+    );
+}
 
 BaseRecipientQueueCard.propTypes = {
     title: PropTypes.string,
