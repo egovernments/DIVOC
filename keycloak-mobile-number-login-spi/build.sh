84ba1773cd30cab3f88b97eacf5913a484a56eff
Dileep Bapat <dileepbapat@gmail.com>
2020-11-29 18:54:03 +0530

    Moving key cloak themes to its own folder for packaging

--- keycloak-mobile-number-login-spi/build.sh
+++ keycloak-mobile-number-login-spi/build.sh
@@ -1,4 +1,5 @@
 #./mvnw clean install
+mkdir -p ../keycloak/providers || echo "exists"
 mkdir -p ../keycloak/providers && cp target/keycloak-mobile-number-login-spi-1.0-SNAPSHOT.jar ../keycloak/providers
-cp -r themes/sys-admin ../keycloak/themes/
-cp -r themes/facility-operator ../keycloak/themes/
+#cp -r themes/sys-admin ../keycloak/themes/
+#cp -r themes/facility-operator ../keycloak/themes/
