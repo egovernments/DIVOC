74480d8ad8747dafc4af8bcdcf50f2bd5907da25
juhij <juhij@sahajsoft.com>
2020-12-01 13:58:12 +0530

    Adding Footer

--- certificate_app/src/App.js
+++ certificate_app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../src/components/Header/Header';
+import Footer from '../src/components/Footer/Footer';
 
 function App() {
   return (
     <div>
         <Header/> 
+        <Footer />
     </div>
   );
 }
