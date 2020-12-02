08e86ae21cd32ad1fb1bbdda331f60991ffed386
juhij <juhij@sahajsoft.com>
2020-12-02 11:16:08 +0530

    Added Login route to download certificate

--- certificate_app/src/App.js
+++ certificate_app/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from '../src/components/Header/Header';
 import Footer from '../src/components/Footer/Footer';
+import {PrivateRoute} from "./utils/PrivateRoute";
+import {useKeycloak} from "@react-keycloak/web";
+import Login from './components/Login';
 
 function App() {
+  const {initialized, keycloak} = useKeycloak();
+    if (!initialized) {
+        return <div>Loading...</div>
+    }
+ 
   return (
     <div>
+      <Router>
         <Header/> 
+        <Switch>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
         <Footer />
+      </Router>     
     </div>
   );
 }
---
+++ certificate_app/src/components/Login.js
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import {useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
+
+import {useKeycloak} from '@react-keycloak/web'
+
+const Login = () => {
+    const {keycloak} = useKeycloak();
+    const history = useHistory();
+
+    useEffect(() => {
+        if (!keycloak.authenticated) {
+            keycloak.login()
+        }
+    }, []);
+
+    useEffect(() => {
+        if (keycloak.authenticated) {
+            let redirectUrl = "/";
+            history.push(redirectUrl)
+        }
+    }, [keycloak]);
+
+    return (
+        <div>
+            Login
+        </div>
+    )
+};
+
+export default Login
