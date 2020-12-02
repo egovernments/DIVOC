5b014285d5fb0935a3432205b14dd5aa3b2ef057
Tejash <tejash.jl1020@gmail.com>
2020-12-02 13:18:10 +0530

    added table view to show facilities, vaccinations and enrollments

--- portal/src/components/FacilitiesRegistry/FacilitiesRegistry.js
+++ portal/src/components/FacilitiesRegistry/FacilitiesRegistry.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import UploadCSV from '../UploadCSV/UploadCSV';
+import {useAxios} from "../../utils/useAxios";
+import {CustomTable} from "../CustomTable";
 
-function Facilities(){
-  const fileUploadAPI = 'divoc/admin/api/v1/facilities';
-  
-  return(
-    <UploadCSV fileUploadAPI={fileUploadAPI}/>
-  );
+function Facilities() {
+    const [facilities, setFacilities] = useState([]);
+    const fileUploadAPI = 'divoc/admin/api/v1/facilities';
+    const axiosInstance = useAxios('');
+
+    useEffect(() => {
+        fetchFacilities()
+    }, []);
+
+    function fetchFacilities() {
+        axiosInstance.current.get(fileUploadAPI)
+            .then(res => {
+                setFacilities(res.data)
+            });
+    }
+
+    return (
+        <div>
+            <UploadCSV fileUploadAPI={fileUploadAPI} onUploadComplete={fetchFacilities}/>
+            <CustomTable data={facilities} fields={[ "serialNum", "facilityName", "status", "admins"]}/>
+        </div>
+    );
 }
+
 export default Facilities;
--- portal/src/components/PreEnrollment/PreEnrollment.js
+++ portal/src/components/PreEnrollment/PreEnrollment.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import UploadCSV from '../UploadCSV/UploadCSV';
+import {useAxios} from "../../utils/useAxios";
+import {CustomTable} from "../CustomTable";
 
 function PreEnrollment(){
+    const [enrollments, setEnrollments] = useState([]);
     const fileUploadAPI = 'divoc/admin/api/v1/enrollments';
+    const axiosInstance = useAxios('');
+    useEffect(() => {
+        fetchEnrollment()
+    }, []);
+
+    function fetchEnrollment() {
+        axiosInstance.current.get(fileUploadAPI)
+            .then(res => {
+                setEnrollments(res.data)
+            });
+    }
     return(
-       <UploadCSV fileUploadAPI={fileUploadAPI}/>
+        <div>
+            <UploadCSV fileUploadAPI={fileUploadAPI} onUploadComplete={fetchEnrollment}/>
+            <CustomTable data={enrollments} fields={["name", "phone", "enrollmentScopeId"]}/>
+        </div>
     );
 }
 export default PreEnrollment;
