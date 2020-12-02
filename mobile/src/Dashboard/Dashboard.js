645006f16121c18fe41dae77c792832c074bb127
Tejash <tejash.jl1020@gmail.com>
2020-12-02 18:44:23 +0530

    fixed footer styles

--- mobile/src/Dashboard/Dashboard.js
+++ mobile/src/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Dashboard.scss'
-import Nav from "react-bootstrap/Nav";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {BottomItem} from "../Base/Base";
-import home from '../Home/home.png'
-import language from '../Language/language.png'
-import queue from '../Queue/queue.png'
-import logout from '../Logout/logout.png'
+import home from '../Home/home.svg'
+import language from '../Language/language.svg'
+import queue from '../Queue/queue.svg'
+import logout from '../Logout/logout.svg'
+import help from '../assets/img/help.svg'
 import {Home} from "../Home/Home";
 import {Queue} from "../Queue/Queue";
 import {SelectLanguage} from "../Language/Language";
@@ -14,12 +14,12 @@ import {Logout} from "../Logout/Logout";
 import {PrivateRoute} from "../utils/PrivateRoute";
 import {LoginComponent} from "../Login/Login";
 import {PreEnrollmentFlow} from "../Home/Forms/PreEnrollmentFlow";
-import {ConfirmFlow, ConfirmVaccination} from "../ConfirmVaccination";
+import {ConfirmFlow} from "../ConfirmVaccination";
+import {useLocation} from "react-router";
 
 Dashboard.propTypes = {};
 
 function Dashboard(props) {
-    const {selectedKey, setSelectedKey} = useState("home")
     return (
         <Router>
             <div className={"dashboard-container"}>
@@ -34,21 +34,25 @@ function Dashboard(props) {
                         <PrivateRoute path="/confirm/vaccination/:recipient_id/:pageName" component={ConfirmFlow}/>
                         <Route path="/login" exact component={LoginComponent}/>
                     </Switch>
+                    <Footer/>
+                </div>
 
-                </div>
-                <div className={"bottom-bar"}>
-                    <Nav justify variant="tabs" defaultActiveKey="/"
-                         activeKey={selectedKey}
-                         onSelect={(k) => setSelectedKey(k)}>
-                        <BottomItem src={home} href={"/"} title={"Home"}/>
-                        <BottomItem src={language} href={"/language"} title={"Language"}/>
-                        <BottomItem src={queue} href={"/queue"} title={"Queue"}/>
-                        <BottomItem src={logout} href={"/logout"} title={"Logout"}/>
-                    </Nav>
-                </div>
             </div>
         </Router>
     );
 }
 
 export default Dashboard;
+
+const Footer = () => {
+    const location = useLocation();
+    return (
+        <div className="bottom-bar d-flex justify-content-around">
+            <BottomItem currentLocation={location.pathname} src={home} href={"/"} title={"Home"}/>
+            <BottomItem currentLocation={location.pathname} src={language} href={"/language"} title={"Language"}/>
+            <BottomItem currentLocation={location.pathname} src={queue} href={"/queue"} title={"Queue"}/>
+            <BottomItem currentLocation={location.pathname} src={help} href={"/logout"} title={"Help"}/>
+            <BottomItem currentLocation={location.pathname} src={logout} href={"/logout"} title={"Logout"}/>
+        </div>
+    )
+}
