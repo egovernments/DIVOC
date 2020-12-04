2a0104f1852b3848d303b47a6f3d3e486078bbcf
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2020-12-04 08:45:57 +0530

    Setup react-intl library

--- mobile/src/Language/Language.js
+++ mobile/src/Language/Language.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import {BaseCard} from "../Base/Base";
 import "./Language.scss"
+import {Button} from "react-bootstrap";
+import {FormattedMessage, FormattedNumber} from "react-intl";
+import * as PropTypes from "prop-types";
+import {useLocale} from "../lang/LocaleContext";
+
+
+function LanguageSelection(props) {
+    return <div className={"language-container"}>
+        <h1 className={"title"}><FormattedMessage id={"app.selectLanguage"} defaultMessage={"Hello"}/></h1>
+        <h1 className={"title"}><FormattedNumber value={106}/></h1>
+        <Button onClick={props.onClick}>Change to hindi</Button>
+        <Button onClick={props.onClick1}>Change to English</Button>
+        <Button onClick={props.onClick2}>Change to Arabic</Button>
+    </div>;
+}
+
+LanguageSelection.propTypes = {
+    onClick: PropTypes.func,
+    onClick1: PropTypes.func
+};
 
 export function SelectLanguage(props) {
+    const locale = useLocale()
     return (
         <BaseCard>
-            <div className={"language-container"}>
-                <h1 className={"title"}>Select Language</h1>
-            </div>
+            <LanguageSelection onClick={() => {
+                locale.selectLanguage("hi")
+            }} onClick1={() => {
+                locale.selectLanguage("en")
+            }} onClick2={() => {
+                locale.selectLanguage("ar")
+            }}/>
         </BaseCard>
     );
 
---
+++ mobile/src/lang/LocaleContext.js
@@ -0,0 +1,52 @@
+import React, {useContext, useState} from 'react';
+import {IntlProvider} from 'react-intl';
+import Hindi from '../lang/hi.json';
+import English from '../lang/en.json';
+import Arabic from '../lang/ar.json';
+
+const LocaleContext = React.createContext(null);
+
+const local = navigator.language;
+
+let lang;
+if (local === 'hi') {
+    lang = Hindi;
+} else if (local === 'ar') {
+    lang = Arabic;
+} else {
+    lang = English;
+}
+
+export function LocaleProvider(props) {
+    const [locale, setLocale] = useState(local);
+    const [messages, setMessages] = useState(lang);
+
+    function selectLanguage(newLocale) {
+        setLocale(newLocale);
+
+        if (newLocale === 'hi') {
+            setMessages(Hindi);
+        } else if (newLocale === 'ar') {
+            setMessages(Arabic);
+        } else {
+            setMessages(English);
+        }
+    }
+
+    return (
+        <LocaleContext.Provider value={{locale, selectLanguage}}>
+            <IntlProvider messages={messages} locale={locale}>
+                {props.children}
+            </IntlProvider>
+        </LocaleContext.Provider>
+
+    );
+}
+
+export function useLocale() {
+    const context = useContext(LocaleContext);
+    const {selectLanguage} = context;
+    return {
+        selectLanguage
+    }
+}
