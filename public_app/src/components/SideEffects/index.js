d8d9e42bc172cf520e7f8ed9d9d3ece5a46b810a
Tejash <tejash.jl1020@gmail.com>
2020-12-09 11:58:07 +0530

    added subgroup for symptoms

--- public_app/src/components/SideEffects/index.js
+++ public_app/src/components/SideEffects/index.js
@@ -6,42 +6,65 @@ import {SubmitSymptomsForm} from "../SubmitSymptomsForm";
 export const SideEffects = () => {
     const [symptoms, setSymptoms] = useState([
         {
-            "name": "Loss of Smell / Taste",
-            "instructions": ["You should get Covid-19 test done."]
+            "name": "Loss of Sense",
+            "types": [
+                {
+                    "name": "Loss of Smell",
+                    "instructions": ["You should get Covid-19 test done."]
+                },
+                {
+                    "name": "Loss of Taste",
+                    "instructions": ["You should get Covid-19 test done."]
+                },
+                {
+                    "name": "Temporary loss of Vision ",
+                    "instructions": ["You should consult a pulmonologist"]
+                },
+            ]
         },
         {
-            "name": "Temporary loss of Vision ",
-            "instructions": ["You should consult a pulmonologist"]
+            "name": "Mental Health",
+            "types": [
+                {
+                    "name": "Memory loss",
+                    "instructions": ["You should consult post-Covid support"]
+                },
+                {
+                    "name": "Bouts of Depression / Anxiety / Panic Attacks",
+                    "instructions": ["You should consult a psychiatrist"]
+                },
+                {
+                    "name": "Severe Headache",
+                    "instructions": ["You should consult post-Covid support"]
+                },
+            ]
         },
         {
-            "name": "Palpitations",
-            "instructions": ["You should consult a cardiologist"]
+            "name": "Cardiovascular disease symptoms",
+            "types": [
+                {
+                    "name": "Sudden black Out",
+                    "instructions": ["You should consult a cardiologist"]
+                },
+                {
+                    "name": "Palpitations",
+                    "instructions": ["You should consult a cardiologist"]
+                },
+            ]
+        }, {
+            "name": "Paralysis",
+            "types": [
+                {
+                    "name": "Temporary Paralysis",
+                    "instructions": ["You should consult a neurologist"]
+                }, {
+                    "name": "Permanent Paralysis",
+                    "instructions": ["You should consult a neurologist"]
+                }
+            ]
         },
-        {
-            "name": "Memory loss",
-            "instructions": ["You should consult post-Covid support"]
-        },
-        {
-            "name": "Chronic Fatigue",
-            "instructions": ["You should consult a pulmonologist"]
-        },
-        {
-            "name": "Sudden black Out",
-            "instructions": ["You should consult a cardiologist"]
-        },
-        {
-            "name": "Bouts of Depression / Anxiety / Panic Attacks",
-            "instructions": ["You should consult a psychiatrist"]
-        },
-        {
-            "name": "Severe Headache",
-            "instructions": ["You should consult post-Covid support"]
-        },
-        {
-            "name": "Temporary Paralysis",
-            "instructions": ["You should consult a neurologist"]
-        }
     ]);
+    const [selectedGroupSymptoms, setSelectedGroupSymptoms] = useState([0]);
     const [selectedSymptoms, setSelectedSymptoms] = useState([]);
     const [showSubmitForm, setShowSubmitForm] = useState(false);
 
@@ -56,6 +79,17 @@ export const SideEffects = () => {
         setSelectedSymptoms(data)
     }
 
+    function onSymptomGroupSelected(symptom) {
+        const data = [...selectedGroupSymptoms];
+        const symptomIndex = data.indexOf(symptom);
+        if (symptomIndex >= 0) {
+            data.splice(symptomIndex, 1);
+        } else {
+            data.push(symptom)
+        }
+        setSelectedGroupSymptoms(data)
+    }
+
     function onOtherSymptomChange(evt) {
 
     }
@@ -65,8 +99,9 @@ export const SideEffects = () => {
     }
 
     function onReset() {
-        setShowSubmitForm(false)
-        setSelectedSymptoms([])
+        setShowSubmitForm(false);
+        setSelectedSymptoms([]);
+        setSelectedGroupSymptoms([0]);
     }
 
     return (
@@ -81,17 +116,32 @@ export const SideEffects = () => {
                                     <h5>Select Symptoms</h5>
                                     <div className="symptoms-container">
                                         {
-                                            symptoms.map(({name}, index) => {
+                                            symptoms.map(({name}, groupIndex) => {
                                                 return (
-                                                    <div key={index}
-                                                         className="symptom-wrapper d-flex align-items-center"
-                                                         onClick={() => {
-                                                             onSymptomSelected(index)
-                                                         }}>
+                                                    <>
+                                                        <div key={groupIndex}
+                                                             className="symptom-wrapper d-flex align-items-center pt-1"
+                                                             onClick={() => {
+                                                                 onSymptomGroupSelected(groupIndex)
+                                                             }}>
+                                                    <span className="custom-group">
+                                                        {selectedGroupSymptoms.includes(groupIndex) ? "-" : "+"}
+                                                    </span>
+                                                            <span className="title">{name}</span>
+                                                        </div>
+                                                        {
+                                                            selectedGroupSymptoms.includes(groupIndex) && symptoms[groupIndex].types.map((type, index) => (
+                                                                <div key={index}
+                                                                     className="symptom-wrapper d-flex align-items-center ml-3"
+                                                                     onClick={() => {
+                                                                         onSymptomSelected(`${groupIndex}-${index}`)
+                                                                     }}>
                                                     <span
-                                                        className={`custom-checkbox ${selectedSymptoms.includes(index) ? 'active' : ''}`}/>
-                                                        <span className="title">{name}</span>
-                                                    </div>
+                                                        className={`custom-checkbox ${selectedSymptoms.includes(`${groupIndex}-${index}`) ? 'active' : ''}`}/>
+                                                                    <span className="title">{type.name}</span>
+                                                                </div>))
+                                                        }
+                                                    </>
                                                 )
                                             })
                                         }
@@ -123,13 +173,16 @@ export const SideEffects = () => {
                                     <div className="instructions-container">
                                         {
                                             selectedSymptoms.map((symptomIdx, idx) => {
-                                                if (symptomIdx in symptoms) {
-                                                    const {name, instructions} = symptoms[symptomIdx];
+                                                const [groupId, symptomId] = symptomIdx.split("-");
+                                                if (groupId in symptoms) {
+                                                    const {types} = symptoms[groupId];
+                                                    const {name, instructions} = types[symptomId];
                                                     return (
                                                         instructions.map((instruction, index) => (
                                                             <div className="instruction-wrapper">
                                                                 <span className="instruction-title">{name}</span><br/>
-                                                                <span className="instruction-heading">Instructions</span>
+                                                                <span
+                                                                    className="instruction-heading">Instructions</span>
                                                                 <br/>
                                                                 <span key={index}>{instruction}</span>
                                                             </div>
@@ -150,4 +203,4 @@ export const SideEffects = () => {
             </Container>
         </div>
     );
-}
+};
