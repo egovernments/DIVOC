9eacc67abccf303873469f843a3c826078cf3743
Tejash <tejash.jl1020@gmail.com>
2020-12-09 19:08:19 +0530

    fixed styling of pre-enrollment

--- portal/src/components/PreEnrollment/PreEnrollment.js
+++ portal/src/components/PreEnrollment/PreEnrollment.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import UploadCSV from '../UploadCSV/UploadCSV';
 import {useAxios} from "../../utils/useAxios";
 import {CustomTable} from "../CustomTable";
+import {TotalRecords} from "../TotalRecords";
 
 function PreEnrollment(){
     const [enrollments, setEnrollments] = useState([]);
@@ -19,7 +20,13 @@ function PreEnrollment(){
     }
     return(
         <div>
-            <UploadCSV fileUploadAPI={fileUploadAPI} onUploadComplete={fetchEnrollment}/>
+            <div className="d-flex mt-3">
+                <UploadCSV fileUploadAPI={fileUploadAPI} onUploadComplete={fetchEnrollment}/>
+                <TotalRecords
+                    title={"Total # of Enrollments in the\nDIVOC Enrollments Registry"}
+                    count={enrollments.length}
+                />
+            </div>
             <CustomTable data={enrollments} fields={["name", "phone", "enrollmentScopeId"]}/>
         </div>
     );
