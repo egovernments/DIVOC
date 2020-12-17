1b16d8dab3c6ece8b145e1b28f507667fa7ef0c6
Tejash <tejash.jl1020@gmail.com>
2020-12-17 18:55:35 +0530

    added initial views for facility and query analysis

--- portal/src/components/Analytics/Anlaytics.js
+++ portal/src/components/Analytics/Anlaytics.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import {CertificateAnalysis} from "../CertificateAnalysis";
 import {Accordion} from "react-bootstrap";
 import {useAxios} from "../../utils/useAxios";
+import {FacilityAnalysis} from "../FacilityAnalysis";
+import {CertificateQueryAnalysis} from "../CertificateQueryAnalysis";
 
 
 export function Analytics() {
@@ -15,9 +17,11 @@ export function Analytics() {
             });
     }, []);
     return (
-        <div className="container-fluid mt-5">
+        <div className="container-fluid mt-5 pb-5">
             <Accordion defaultActiveKey="0">
                 {analytics && <CertificateAnalysis analytics={analytics}/>}
+                {analytics && <FacilityAnalysis analytics={analytics}/>}
+                {analytics && <CertificateQueryAnalysis analytics={analytics}/>}
             </Accordion>
         </div>
     )
