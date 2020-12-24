7a734b281308b3e39fce206b319fda71fcc73949
Tejash <tejash.jl1020@gmail.com>
2020-12-24 11:14:17 +0530

    integrated schema api

--- public_app/src/components/SideEffects/index.js
+++ public_app/src/components/SideEffects/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import "./index.css";
 import {useHistory} from "react-router-dom";
@@ -11,111 +11,20 @@ import VerifyCertificateImg from '../../assets/img/verify-certificate-home.svg'
 import LearnMoreImg from '../../assets/img/leanr_more_small.png'
 import {useKeycloak} from "@react-keycloak/web";
 import {RECIPIENT_CLIENT_ID, RECIPIENT_ROLE, SIDE_EFFECTS_DATA} from "../../constants";
+import axios from "axios";
 
 export const SideEffects = () => {
     const history = useHistory();
     const {keycloak} = useKeycloak();
-    const schema = {
-        properties: {
-            "Flu-like symptoms": {
-                "type": "boolean",
-                "title": "Flu-like symptoms",
-                "enumNames": ["Yes", "No"]
-            },
-            "Temperature": {
-                "type": "number",
-                "title": "Temperature",
-                "minimum": 90,
-                "maximum": 108,
-                "multipleOf": 0.1,
-                "unit": "°F"
-            },
-            "Rapid Heartbeat": {
-                "type": "boolean",
-                "title": "Rapid Heartbeat",
-                "enumNames": ["Yes", "No"]
-            },
-            "Fatigue": {
-                "type": "boolean",
-                "title": "Fatigue",
-                "enumNames": ["Yes", "No"]
-            },
-            "Headache": {
-                "type": "boolean",
-                "title": "Headache",
-                "enumNames": ["Yes", "No"]
-            },
-            "Muscle/Joint Pain": {
-                "type": "boolean",
-                "title": "Muscle/Joint Pain",
-                "enumNames": ["Yes", "No"]
-            },
-            "Pain Scale": {
-                "type": "number",
-                "title": "Pain Scale",
-                "minimum": 0,
-                "maximum": 10,
-                "multipleOf": 1,
-                "unit": ""
-            },
-            "Chills": {
-                "type": "boolean",
-                "title": "Chills",
-                "enumNames": ["Yes", "No"]
-            },
-            "Cough": {
-                "type": "boolean",
-                "title": "Cough",
-                "enumNames": ["Yes", "No"]
-            },
-            "Paralysis": {
-                "type": "boolean",
-                "title": "Paralysis",
-                "enumNames": ["Yes", "No"]
-            },
-            "Arm Soreness": {
-                "type": "boolean",
-                "title": "Arm Soreness",
-                "enumNames": ["Yes", "No"]
-            },
-            "Nausea": {
-                "type": "boolean",
-                "title": "Nausea",
-                "enumNames": ["Yes", "No"]
-            },
-            "Migraine": {
-                "type": "boolean",
-                "title": "Migraine",
-                "enumNames": ["Yes", "No"],
-            },
-            "Swollen Glands": {
-                "type": "boolean",
-                "title": "Swollen Glands",
-                "enumNames": ["Yes", "No"]
-            },
-            "Other": {
-                "className": "feedback-input-box",
-                "type": "string"
-            }
-
-        }
-    };
-
-    const uiSchema = {
-        "Temperature": {
-            "ui:options": {label: false},
-            "ui:widget": "range"
-        },
-        "Pain Scale": {
-            "ui:options": {label: false},
-            "ui:widget": "range"
-        },
-        "Other": {
-            "classNames": ["side-effects-input-box"],
-            "ui:options": {label: false},
-            "ui:placeholder": "Type to enter any other conditions"
-        }
-    };
+    const [formSchema, setFormSchema] = useState();
+    useEffect(() => {
+        axios
+            .get("/divoc/api/v1/sideEffects")
+            .then((res) => {
+                setFormSchema(res.data);
+                return res.data;
+            });
+    }, []);
 
     const widgets = {
         CheckboxWidget: CustomCheckboxWidget,
@@ -129,7 +38,7 @@ export const SideEffects = () => {
         }
     }, []);
     const onSideEffectsSubmit = async ({formData}, e) => {
-        if(Object.keys(formData).length > 0 ) {
+        if (Object.keys(formData).length > 0) {
             localStorage.setItem(SIDE_EFFECTS_DATA, JSON.stringify(formData));
             history.push("/feedback/verify")
         } else {
@@ -148,8 +57,8 @@ export const SideEffects = () => {
                             <Col>
                                 <h4 align="">Report Side-effects</h4>
                                 <h5 align="">Select Symptoms</h5>
-                                <Form schema={schema}
-                                      uiSchema={uiSchema} widgets={widgets} onSubmit={onSideEffectsSubmit}>
+                                <Form schema={formSchema.schema}
+                                      uiSchema={formSchema.uiSchema} widgets={widgets} onSubmit={onSideEffectsSubmit}>
                                     <div className="d-flex justify-content-center">
                                         <CustomButton className="green-btn" type="submit" onClick={() => {
                                         }}>
