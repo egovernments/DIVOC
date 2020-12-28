28686b93abc6cb067c299345f0dbd54ac70029e2
Tejash <tejash.jl1020@gmail.com>
2020-12-28 19:09:28 +0530

    initial notification service app

---
+++ notification-service/cmd/notification-service/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/divoc/notification-service/swagger_gen/restapi"
+	"github.com/divoc/notification-service/swagger_gen/restapi/operations"
+	"github.com/go-openapi/loads"
+	"github.com/jessevdk/go-flags"
+	"log"
+	"os"
+)
+
+func main() {
+	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewNotificationServiceAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	defer server.Shutdown()
+
+	parser := flags.NewParser(server, flags.Default)
+	parser.ShortDescription = "Divoc Portal API"
+	parser.LongDescription = "Digital infra for vaccination certificates"
+	server.ConfigureFlags()
+	for _, optsGroup := range api.CommandLineOptionsGroups {
+		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if _, err := parser.Parse(); err != nil {
+		code := 1
+		if fe, ok := err.(*flags.Error); ok {
+			if fe.Type == flags.ErrHelp {
+				code = 0
+			}
+		}
+		os.Exit(code)
+	}
+
+	server.ConfigureAPI()
+
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ notification-service/go.mod
@@ -0,0 +1,19 @@
+module github.com/divoc/notification-service
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-openapi/errors v0.19.9
+	github.com/go-openapi/loads v0.20.0
+	github.com/go-openapi/runtime v0.19.24
+	github.com/go-openapi/spec v0.20.0
+	github.com/go-openapi/strfmt v0.19.11
+	github.com/go-openapi/swag v0.19.12
+	github.com/go-openapi/validate v0.20.0
+	github.com/gospotcheck/jwt-go v4.0.0+incompatible
+	github.com/jessevdk/go-flags v1.4.0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
+)
+
+replace github.com/divoc/kernel_library => ../kernel_library
