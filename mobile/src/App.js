6a13eaaa595034daaf348ebc5f99c9a5b03511b3
Dileep Bapat <dileepbapat@gmail.com>
2020-12-31 18:00:47 +0530

    Facility app stale session logout handling

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -3,22 +3,29 @@ import './App.scss';
 import Dashboard from "./Dashboard/Dashboard";
 import {useKeycloak} from "@react-keycloak/web";
 import {SyncFacade} from "./SyncFacade";
+import config from "./config"
 
 function App() {
     const {keycloak, initialized} = useKeycloak();
     const [isDBInit, setDBInit] = useState(false);
     useEffect(() => {
-        if (initialized && keycloak.authenticated) {
-            localStorage.setItem("token", keycloak.token)
-            SyncFacade.pull().then(value => {
-                setDBInit(true)
-            }).catch((e) => {
-                console.log(e)
-                setDBInit(true)
-            });
+        if (initialized) {
+            if (keycloak.authenticated) {
+                localStorage.setItem("token", keycloak.token);
+                SyncFacade.pull().then(value => {
+                    setDBInit(true)
+                }).catch((e) => {
+                    console.log(e);
+                    setDBInit(true)
+                });
+            } else {
+                if (!keycloak.authenticated) {
+                    keycloak.login({redirectUri:config.urlPath})
+                }
+            }
         }
-    }, [initialized])
-    if (!initialized) {
+    }, [initialized]);
+    if (!initialized || !isDBInit) {
         return <div>Loading...</div>
     }
 
