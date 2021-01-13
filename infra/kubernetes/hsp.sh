ca5b8f2f55682dc02035873f1695d98ddabd866f
Dileep Bapat <dileepbapat@gmail.com>
2021-01-13 16:48:05 +0530

    Adding kubernetes deployment config

---
+++ infra/kubernetes/hsp.sh
@@ -0,0 +1,4 @@
+kubectl -n divoc autoscale deployment  certificate-signer --cpu-percent=70 --min=1 --max=10
+kubectl -n divoc autoscale deployment  registry --cpu-percent=70 --min=1 --max=10
+kubectl -n divoc autoscale deployment  vaccination-api --cpu-percent=70 --min=1 --max=10
+kubectl -n ingress-nginx autoscale deployment  ingress-nginx-controller --cpu-percent=70 --min=1 --max=10
