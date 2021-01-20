156c4ce0bc21824a7c36bb98c3d89fb897ae5859
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2021-01-20 16:42:27 +0530

    Show sync now UI on home

--- mobile/src/SyncFacade.js
+++ mobile/src/SyncFacade.js
@@ -49,4 +49,36 @@ export class SyncFacade {
         }
         return false;
     }
+
+    static lastSyncedOn() {
+        const lastSyncDate = localStorage.getItem(LAST_SYNC_KEY);
+        const lastSyncTime = new Date(lastSyncDate).getTime()
+        const currentTime = new Date().getTime()
+        return relativeTimeDifference(currentTime, lastSyncTime)
+    }
+}
+
+function relativeTimeDifference(current, previous) {
+
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const msPerYear = msPerDay * 365;
+
+    const elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+        return Math.round(elapsed / 1000) + ' seconds ago';
+    } else if (elapsed < msPerHour) {
+        return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    } else if (elapsed < msPerDay) {
+        return Math.round(elapsed / msPerHour) + ' hours ago';
+    } else if (elapsed < msPerMonth) {
+        return Math.round(elapsed / msPerDay) + ' days ago';
+    } else if (elapsed < msPerYear) {
+        return Math.round(elapsed / msPerMonth) + ' months ago';
+    } else {
+        return Math.round(elapsed / msPerYear) + ' years ago';
+    }
 }
