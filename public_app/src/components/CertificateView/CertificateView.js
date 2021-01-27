3f7910f71a9e8ada724df05091ddf3bb880a387a
juhij <juhij@sahajsoft.com>
2021-01-27 11:51:01 +0530

    Fixed certificate alignment

--- public_app/src/components/CertificateView/CertificateView.js
+++ public_app/src/components/CertificateView/CertificateView.js
@@ -214,7 +214,7 @@ function CertificateView() {
     const selectedCertificate = (certificateData) => {
         return <>
             {showCertificatePreview(certificateData)}
-            <div className={styles["top-pad"] + " " + styles["no-print"]}>
+            <div className={styles["top-pad"] + " " + styles["no-print"] + " "+ styles["center-align"]}>
                 <div >
                     {/*<button className={styles["button"]} onClick={handleClick}>*/}
                     {/*    Download Certificate <img src={DownloadLogo} alt="download"/>*/}
@@ -257,7 +257,7 @@ function CertificateView() {
     const multiCertificateView = () => {
         return (
             <div>
-                <div className={styles["no-print"]}>
+                <div className={styles["no-print"] + " " + styles["center-align"]}>
                     <p>There are multiple certificates associated with phone : {userMobileNumber}</p>
                     <b>Please choose the certificate for </b>
                     <div>{getListOfCertificateBearers()}</div>
@@ -273,8 +273,8 @@ function CertificateView() {
         <div className={"row-cols-lg-1 row-cols-1 nav-pad cert-top"}>
             <div className="col-12 d-flex d-flex justify-content-center">
                 <div>
-                    <div className={styles["no-print"]}>
-                        <p>Vaccination certificate</p>
+                    <div className={styles["no-print"] + " " + styles["center-align"]}>
+                        <h4>Vaccination certificate</h4>
                     </div>
                     {(certificateList.length > 1) ? multiCertificateView() : singleCertificateView()}
                 </div>
