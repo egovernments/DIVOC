92c3f4e0f69c9c163045ebfca383e975840da32e
Tejash <tejash.jl1020@gmail.com>
2021-02-02 11:50:56 +0530

    fixed ui issues

--- mobile/src/utils/date_utils.js
+++ mobile/src/utils/date_utils.js
@@ -1,5 +1,5 @@
 export function formatCertifyDate(dob) {
-    let day = dob.getDate();
+    let day = dob.getDate().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
     let month = (dob.getMonth() + 1).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
     let year = dob.getFullYear();
     return `${year}-${month}-${day}`;
