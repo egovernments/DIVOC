1a63b986b536390ad2b2030f94383688436eda14
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2021-02-02 12:10:20 +0530

    Vaccinator react selector

--- mobile/src/components/SelectVaccinator/index.js
+++ mobile/src/components/SelectVaccinator/index.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import {ACTION_PATIENT_COMPLETED, useConfirmVaccine} from "../../ConfirmVaccination";
 import DropdownItem from "react-bootstrap/DropdownItem";
 import {SyncFacade} from "../../SyncFacade";
+import Select from 'react-select'
 
 export const SelectVaccinator = (props) => {
     const {markPatientComplete, getFormDetails, goNext} = useConfirmVaccine();
@@ -34,6 +35,16 @@ export const SelectVaccinator = (props) => {
         }
     }
 
+    function defaultVaccinatorOption() {
+        const defaultVaccinator = vaccinators.find((item) => item.osid === selectedVaccinatorId)
+        if (defaultVaccinator) {
+            return {
+                label: defaultVaccinator.name,
+                value: defaultVaccinator.osid,
+            }
+        }
+    }
+
     function onBatchCodeChange(e) {
         setBatchCode(e.target.value)
     }
@@ -50,23 +61,23 @@ export const SelectVaccinator = (props) => {
                 setSelectedBatchId(result.selectedBatchId)
             })
     }, [])
+
     return (
         <div className="select-vaccinator-wrapper">
             <div className="table-wrapper">
                 <span className="select-title">SELECT VACCINATOR</span>
-                <DropdownButton id="dropdown-item-button" title="Dropdown button">
-                    {
-                        vaccinators.map((data, index) => {
-                            if (selectedVaccinatorId && selectedVaccinatorId === data.osid) {
-                                return <DropdownItem as="button" active>{data.name}</DropdownItem>
-                            } else {
-                                return <DropdownItem as="button"
-                                                     onClick={() => setSelectedVaccinatorId(data.osid)}>
-                                    {data.name}</DropdownItem>
-                            }
-                        })
-                    }
-                </DropdownButton>
+                <Select
+                    key={selectedVaccinatorId}
+                    defaultValue={defaultVaccinatorOption()}
+                    options={vaccinators.map((item, index) => {
+                        return {
+                            label: item.name,
+                            value: item.osid,
+                        }
+                    })}
+                    onChange={(option) => {
+                        setSelectedVaccinatorId(option.value)
+                    }}/>
                 <span className="select-title">SELECT VACCINE</span>
                 <DropdownButton id="dropdown-item-button" title="Dropdown button">
                     {
