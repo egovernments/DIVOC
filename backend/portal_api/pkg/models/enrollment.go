6ced250a593b97214641662e719eec285e3c4fac
Tejash <tejash.jl1020@gmail.com>
2021-02-04 14:10:59 +0530

    added dob format check for enrollment struct

--- backend/portal_api/pkg/models/enrollment.go
+++ backend/portal_api/pkg/models/enrollment.go
@@ -1,18 +1,32 @@
 package models
 
-import "github.com/divoc/portal-api/swagger_gen/models"
+import (
+	"encoding/json"
+	"github.com/divoc/portal-api/swagger_gen/models"
+	"github.com/pkg/errors"
+	"time"
+)
 
 //mobile,enrollmentScopeId,nationalId,dob,gender,name,email
 type Enrollment struct {
-	Phone             string         `json:"phone"`
-	EnrollmentScopeId string         `json:"enrollmentScopeId"`
-	NationalId        string         `json:"nationalId"`
-	Dob               string         `json:"dob"`
-	Gender            string         `json:"gender"`
-	Name              string         `json:"name"`
-	Email             string         `json:"email"`
-	Code              string         `json:"code"`
-	Certified         bool           `json:"certified"`
-	ProgramId         string         `json:"programId"`
+	Phone             string          `json:"phone"`
+	EnrollmentScopeId string          `json:"enrollmentScopeId"`
+	NationalId        string          `json:"nationalId"`
+	Dob               string          `json:"dob"`
+	Gender            string          `json:"gender"`
+	Name              string          `json:"name"`
+	Email             string          `json:"email"`
+	Code              string          `json:"code"`
+	Certified         bool            `json:"certified"`
+	ProgramId         string          `json:"programId"`
 	Address           *models.Address `json:"address"`
 }
+
+func (e Enrollment) MarshalJSON() ([]byte, error) {
+	_, err := time.Parse("2006-01-02", e.Dob)
+	if err != nil {
+		return nil, errors.New("Detail: DoB is not in required format (Ex: 2006-01-21)")
+	}
+	return json.Marshal(e)
+}
+
