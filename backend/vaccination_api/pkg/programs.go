66230fa796fbb30aa11795cd841b71c3ed2485f1
Tejash <tejash.jl1020@gmail.com>
2021-02-08 18:01:14 +0530

    replaces system fields as per new design

--- backend/vaccination_api/pkg/programs.go
+++ backend/vaccination_api/pkg/programs.go
@@ -12,13 +12,13 @@ import (
 type ProgramsRegistryResponse struct {
 	EndDate     string        `json:"endDate"`
 	Type        string        `json:"@type"`
-	OsCreatedAt time.Time     `json:"_osCreatedAt"`
-	OsUpdatedBy string        `json:"_osUpdatedBy"`
+	OsCreatedAt time.Time     `json:"osCreatedAt"`
+	OsUpdatedBy string        `json:"osUpdatedBy"`
 	Name        string        `json:"name"`
 	Description string        `json:"description"`
 	Osid        string        `json:"osid"`
-	OsCreatedBy string        `json:"_osCreatedBy"`
-	OsUpdatedAt time.Time     `json:"_osUpdatedAt"`
+	OsCreatedBy string        `json:"osCreatedBy"`
+	OsUpdatedAt time.Time     `json:"osUpdatedAt"`
 	MedicineIds []interface{} `json:"medicineIds"`
 	StartDate   string        `json:"startDate"`
 }
