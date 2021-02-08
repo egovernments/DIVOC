3b6f0a04be4d1ef280b783884c71302ee2e37d0c
Tejash <tejash.jl1020@gmail.com>
2021-02-08 19:52:21 +0530

    refactored program and vaccine creation

--- portal/src/components/ListView/ListView.js
+++ portal/src/components/ListView/ListView.js
@@ -1,14 +1,12 @@
 import React, {useState} from 'react';
 import './ListView.css';
-import ProgramActiveImg from "../../assets/img/program-active.svg";
-import Program from "../../assets/img/program.svg";
-import ProgramInActiveImg from "../../assets/img/program-inactive.svg";
+import AddIconImg from "../../assets/img/add-icon.svg";
 import Button from 'react-bootstrap/Button';
 import Form from "@rjsf/core";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Switch from "@material-ui/core/Switch/Switch";
 
-function ListView({listData, fields,show,setShow,title,buttonTitle,schema,uiSchema,widgets,showDetails,onEdit,setSelectedData,autoFillForm}) {
+function ListView({listData, fields, show, setShow, title, buttonTitle, schema, uiSchema, widgets, showDetails, onEdit, setSelectedData, autoFillForm}) {
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     const CustomSwitch = withStyles({
@@ -28,56 +26,84 @@ function ListView({listData, fields,show,setShow,title,buttonTitle,schema,uiSche
         <div>
             {
                 selectedIndex === -1 && <>
-                 <div className="d-flex">
-                    <p className={" p-2 mr-auto"}>{title}</p>
-                    <Button variant="outline-primary" onClick={()=> setShow(!show)}>{buttonTitle}</Button>
-                 </div>
-                 <div className="cards-container">
-                 {listData.map((data, index) => {
-                    return (
-                        <div className={'list-view-card-container'} >
-                            <div className="d-flex justify-content-between" onClick={() => {setSelectedIndex(index);setSelectedData(data)}}>
-                                <span className={'list-view-name'}>{data.name}</span>
-                                {showDetails && <span className={'list-view-logo-img'}>
+                    <div className="d-flex">
+                        <p className="font-weight-bold p-2 mr-auto ">{title}</p>
+                        {buttonTitle &&
+                        <Button variant="outline-primary" className="d-flex align-items-center justify-content-between"
+                                style={{height: "3rem", textTransform: "uppercase"}} onClick={() => setShow(!show)}>
+                            <img className="mr-1" src={AddIconImg}/> {buttonTitle}</Button>}
+                    </div>
+                    <div className="cards-container">
+                        {listData.map((data, index) => {
+                            return (
+                                <div className='list-view-card-container'>
+                                    <div className="d-flex justify-content-between" onClick={() => {
+                                        setSelectedIndex(index);
+                                        setSelectedData(data)
+                                    }}>
+                                        <span className={'list-view-name'}>{data.name}</span>
+                                        {
+                                            showDetails && <span className='list-view-logo-img'>
                                     {"image" in data ? <img alt="" src={data.image} width={"100%"}/> : "LOGO"}
-                                    <img src={data.status === "Active" ? ProgramActiveImg : ProgramInActiveImg}
-                                            className={'list-view-program-status-img'} alt={data.status}
-                                            title={data.status}/>
-                                </span>}
-                            </div>
-                            {!showDetails && <span className="">{data.provider}</span>}
-                            {showDetails && 
-                                <>
-                                <div>{data.description}</div>
-                                <div className="additional-details-card">
-                                    <div className="d-flex">
-                                        <span>Start Date</span>&emsp;&emsp;
-                                        <span>End Date</span>
+
+                                </span>
+                                        }
                                     </div>
-                                    <div className="d-flex">
-                                        <span><b>{data.startDate}</b></span>&emsp;
-                                        <span><b>{data.endDate}</b></span>
+                                    {!showDetails &&
+                                    <div className="d-flex justify-content-between">
+                                        <span className="">{data.provider}</span>
+                                        <div className="custom-switch d-flex flex-column align-items-center"
+                                             style={{fontSize: "12px"}}>
+                                            <CustomSwitch
+                                                checked={data.status === "Active" || false}
+                                                onChange={() => {
+                                                    setSelectedData(data);
+                                                    let editedData = Object.assign({}, data);
+                                                    editedData.status = editedData.status === "Active" ? "Inactive" : "Active";
+                                                    onEdit(editedData)
+                                                }}
+                                                color="primary"
+                                            />
+                                            <p>{data.status === "Active" ? "Active" : "Inactive"}</p>
+                                        </div>
                                     </div>
+                                    }
+                                    {showDetails &&
+                                    <>
+                                        <div className="additional-details"
+                                             title={data.description}>{data.description > 300 ? data.description.substr(0, 300) + "..." : data.description}</div>
+                                        <div className="additional-details-card">
+                                            <div>
+                                                <div className="d-inline-flex flex-column">
+                                                    <span>Start Date</span>
+                                                    <span><b>{data.startDate}</b></span>
+                                                </div>
+                                                <div className="d-inline-flex flex-column ml-3">
+                                                    <span>End Date</span>
+                                                    <span><b>{data.endDate}</b></span>
+                                                </div>
+                                            </div>
+                                            <div className="custom-switch d-inline-flex flex-column align-items-center">
+                                                <CustomSwitch
+                                                    checked={data.status === "Active" || false}
+                                                    onChange={() => {
+                                                        setSelectedData(data);
+                                                        let editedData = Object.assign({}, data);
+                                                        editedData.status = editedData.status === "Active" ? "Inactive" : "Active";
+                                                        onEdit(editedData)
+                                                    }}
+                                                    color="primary"
+                                                />
+                                                <p>{data.status === "Active" ? "Active" : "Inactive"}</p>
+                                            </div>
+                                        </div>
+                                    </>
+                                    }
+
                                 </div>
-                                </>
-                            }
-                            <div className="custom-switch">
-                                <CustomSwitch
-                                checked={data.status==="Active" || false}
-                                onChange={() => {
-                                    setSelectedData(data);
-                                    let editedData =  Object.assign({}, data);
-                                    editedData.status = editedData.status==="Active" ? "Inactive" : "Active" ;
-                                    onEdit(editedData)
-                                }}
-                                color="primary"
-                            />
-                            <p>{data.status === "Active" ? "Active" : "Inactive"}</p>
-                            </div>
-                        </div>
-                    )
-                })}
-                 </div>
+                            )
+                        })}
+                    </div>
                 </>
             }
             {
@@ -86,7 +112,8 @@ function ListView({listData, fields,show,setShow,title,buttonTitle,schema,uiSche
                     <div className={"list-view-selected-container"}>
                         <div className="d-flex justify-content-between align-items-center">
                             <span className={'list-view-name'}>{listData[selectedIndex].name}</span>
-                            <button className="mt-3 list-selected-back-btn" onClick={() => setSelectedIndex(-1)}>BACK</button>
+                            <button className="mt-3 list-selected-back-btn" onClick={() => setSelectedIndex(-1)}>BACK
+                            </button>
                         </div>
                         <div className="form-container">
                             <Form
--- portal/src/components/VaccineRegistration/VaccineRegistration.js
+++ portal/src/components/VaccineRegistration/VaccineRegistration.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./VaccineRegistration.module.css";
-import { useKeycloak } from "@react-keycloak/web";
+import {useKeycloak} from "@react-keycloak/web";
 import axios from "axios";
 import ListView from '../ListView/ListView';
 import Form from "@rjsf/core";
@@ -12,15 +12,15 @@ import {CustomTextAreaWidget} from "../CustomTextAreaWidget/index";
 
 
 function VaccineRegistration() {
-    const { keycloak } = useKeycloak();
+    const {keycloak} = useKeycloak();
     const [formData, setFormData] = useState(null);
     const [medicineList, setMedicineList] = useState([]);
     const [showForm, setShowForm] = useState(false);
-    const [selectedMedicine,setSelectedMedicine] = useState([]);
+    const [selectedMedicine, setSelectedMedicine] = useState([]);
 
     useEffect(() => {
         getListOfRegisteredVaccines();
-    },[]);
+    }, []);
 
     const config = {
         headers: {
@@ -46,29 +46,29 @@ function VaccineRegistration() {
     const handleSubmit = (dataToSend) => {
         if (dataToSend.edited) {
             axios
-            .put("/divoc/admin/api/v1/medicines", dataToSend, config)
-            .then((res) => {
-                alert("Successfully Edited");
-                getListOfRegisteredVaccines()
-            });
+                .put("/divoc/admin/api/v1/medicines", dataToSend, config)
+                .then((res) => {
+                    alert("Successfully Edited");
+                    getListOfRegisteredVaccines()
+                });
         } else {
             axios
-            .post("/divoc/admin/api/v1/medicines", dataToSend, config)
-            .then((res) => {
-                alert("Successfully Registered");
-                getListOfRegisteredVaccines()
-            });
+                .post("/divoc/admin/api/v1/medicines", {...dataToSend, status: "Active"}, config)
+                .then((res) => {
+                    alert("Successfully Registered");
+                    getListOfRegisteredVaccines()
+                });
+            setShowForm(!showForm)
         }
     };
 
-    
 
     const getListOfRegisteredVaccines = async () => {
         const res = await axios
             .get("/divoc/admin/api/v1/medicines", config)
-            .then( (res) => {
+            .then((res) => {
                 return res.data.map(d => {
-                    return {...d,edited: false}
+                    return {...d, edited: false}
                 })
             })
         setMedicineList(res)
@@ -76,14 +76,14 @@ function VaccineRegistration() {
 
     function onEdit(data) {
         data.edited = true;
-        console.log("data to send",data)
+        console.log("data to send", data)
         setSelectedMedicine(data);
         handleSubmit(data);
         getListOfRegisteredVaccines();
     }
 
     function autoFillForm() {
-        return { 
+        return {
             osid: selectedMedicine.osid,
             name: selectedMedicine.name,
             effectiveUntil: selectedMedicine.effectiveUntil,
@@ -92,43 +92,81 @@ function VaccineRegistration() {
             schedule: selectedMedicine.schedule,
             status: selectedMedicine.status,
         }
-    } 
+    }
 
+    let blockedVaccines = medicineList.filter(data => data.status === "Blocked");
+    let inactiveVaccines = medicineList.filter(data => data.status === "Inactive");
     return (
         <div className={styles["container"]}>
             {showForm && <div className={styles["form-container"]}>
-            <div className="d-flex">
-                <h5 className={"mr-auto"}>Register New Vaccine</h5>
-                <Button variant="outline-primary" onClick={()=> setShowForm(!showForm)}>BACK</Button>
-            </div>
-            <Form
-                widgets={widgets}
-                schema={schema}
-                uiSchema={uiSchema}
-                onSubmit={(e) => {
-                    // setFormData(e.formData);
-                    handleSubmit(e.formData);
-                }}
-            >
-                <button type="submit" className={styles['button']}>SAVE</button>
-            </Form>
+                <div className="d-flex">
+                    <h5 className={"mr-auto"}>Register New Vaccine</h5>
+                    <Button variant="outline-primary" onClick={() => setShowForm(!showForm)}>BACK</Button>
+                </div>
+                <Form
+                    widgets={widgets}
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    formData={formData}
+                    onChange={(e) => {
+                        setFormData(e.formData)
+                    }}
+                    onSubmit={(e) => {
+                        // setFormData(e.formData);
+                        handleSubmit(e.formData);
+                    }}
+                >
+                    <button type="submit" className={styles['button']}>SAVE</button>
+                </Form>
             </div>}
             {!showForm && <div className={styles["sub-container"]}>
-            <ListView 
-                schema={schema}
-                uiSchema={uiSchema}
-                widgets={widgets}
-                listData={medicineList} 
-                fields={["provider", "price", "effectiveUntil"]} 
-                show={showForm} 
-                setShow={setShowForm}
-                buttonTitle="Register New Vaccine"
-                title="Active Vaccines"
-                showDetails={false}
-                autoFillForm={autoFillForm}
-                onEdit={onEdit}
-                setSelectedData={setSelectedMedicine}
-            />
+                <ListView
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    widgets={widgets}
+                    listData={medicineList.filter(data => data.status === "Active")}
+                    fields={["provider", "price", "effectiveUntil"]}
+                    show={showForm}
+                    setShow={setShowForm}
+                    buttonTitle="Register New Vaccine"
+                    title="Active Vaccines"
+                    showDetails={false}
+                    autoFillForm={autoFillForm}
+                    onEdit={onEdit}
+                    setSelectedData={setSelectedMedicine}
+                />
+                {inactiveVaccines.length > 0 && <><div className="mt-3"/>
+                <ListView
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    widgets={widgets}
+                    listData={inactiveVaccines}
+                    fields={["provider", "price", "effectiveUntil"]}
+                    show={showForm}
+                    setShow={setShowForm}
+                    buttonTitle=""
+                    title="Inactive Vaccines"
+                    showDetails={false}
+                    autoFillForm={autoFillForm}
+                    onEdit={onEdit}
+                    setSelectedData={setSelectedMedicine}
+                /></>}
+                {blockedVaccines.length > 0 && <><div className="mt-3"/>
+                <ListView
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    widgets={widgets}
+                    listData={blockedVaccines}
+                    fields={["provider", "price", "effectiveUntil"]}
+                    show={showForm}
+                    setShow={setShowForm}
+                    buttonTitle=""
+                    title="Blocked Vaccines"
+                    showDetails={false}
+                    autoFillForm={autoFillForm}
+                    onEdit={onEdit}
+                    setSelectedData={setSelectedMedicine}
+                /></>}
             </div>}
         </div>
     );
