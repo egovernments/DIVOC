eba5c3f8b01f4f27564a3748b8dae141743ae97a
Tejash <tejash.jl1020@gmail.com>
2021-02-10 21:58:04 +0530

    added facility details to nav bar for facility users

--- portal/src/components/Header/index.js
+++ portal/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarLogo from "../../assets/img/nav-logo.png";
+import ProfileImg from "../../assets/img/profile.png";
 import "./index.css";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -12,6 +13,19 @@ import {useSelector} from "react-redux";
 export const Header = (props) => {
     const {keycloak} = useKeycloak();
     const logo = useSelector(state => state.flagr.appConfig.applicationLogo);
+    const facility = useSelector(state => state.facility);
+    const isFacilityUser = () => {
+        return keycloak.hasResourceRole(CONSTANTS.FACILITY_ADMIN_ROLE, CONSTANTS.PORTAL_CLIENT) ||
+            keycloak.hasResourceRole(CONSTANTS.FACILITY_PRINT_STAFF, CONSTANTS.PORTAL_CLIENT)
+    };
+
+    function getFacilityAddress() {
+        if ("address" in facility && facility.address) {
+            return [facility.address.district.trim(), facility.address.state.trim()].filter(s => s !== "").join(", ");
+        } else {
+            return ""
+        }
+    }
 
     return (
         <Navbar fixed="top" bg="white">
@@ -25,7 +39,16 @@ export const Header = (props) => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                <Nav className="">
+                <Nav className="align-items-center">
+                    {
+                        isFacilityUser() && facility && <div className="d-flex align-items-center" style={{fontSize: "14px"}}>
+                            <img src={ProfileImg}/>
+                            <div className="d-flex flex-column ml-2 mr-2">
+                                <b>{facility.facilityName}</b>
+                                <span>{getFacilityAddress()}</span>
+                            </div>
+                        </div>
+                    }
                     {/*{!keycloak.authenticated && <Nav.Link href="#home">MAP</Nav.Link>}*/}
                     {/*<Nav.Link href="https://divoc.xiv.in" target="_blank">PUBLIC PORTAL</Nav.Link>*/}
                     {keycloak.authenticated && keycloak.hasResourceRole(CONSTANTS.MONITORING, CONSTANTS.PORTAL_CLIENT) &&
--- portal/src/redux/reducers/index.js
+++ portal/src/redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
 import {flagrConfigReducer} from "./flagrConfig";
+import {facilityReducer} from "./facilityReducer";
 
-export default combineReducers({flagr: flagrConfigReducer})
+export default combineReducers({flagr: flagrConfigReducer, facility: facilityReducer})
