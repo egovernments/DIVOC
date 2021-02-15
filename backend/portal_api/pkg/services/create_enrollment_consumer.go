cca8a876dcc0bf51e64aab7d8c1097294bdf24fe
kesavanpalani <kesavanpalanipk@gmail.com>
2021-02-15 10:20:22 +0530

    Send notification on successful enrolment

--- backend/portal_api/pkg/services/create_enrollment_consumer.go
+++ backend/portal_api/pkg/services/create_enrollment_consumer.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"github.com/divoc/portal-api/config"
 	"github.com/divoc/portal-api/pkg/models"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 	"time"
 )
@@ -18,7 +18,7 @@ func StartEnrollmentConsumer() {
 		"enable.auto.commit": "false",
 	})
 	if err != nil {
-		logrus.Errorf("Failed connecting to kafka", err)
+		log.Errorf("Failed connecting to kafka", err)
 	}
 	go func() {
 		err := consumer.SubscribeTopics([]string{config.Config.Kafka.EnrollmentTopic}, nil)
@@ -28,28 +28,32 @@ func StartEnrollmentConsumer() {
 		for {
 			msg, err := consumer.ReadMessage(-1)
 			if err == nil {
-				logrus.Info("Got the message to create new enrollment")
+				log.Info("Got the message to create new enrollment")
 				var enrollment models.Enrollment
 				err = json.Unmarshal(msg.Value, &enrollment)
 
 				if err == nil {
 					_, err := time.Parse("2006-01-02", enrollment.Dob)
-					logrus.Infof("Message on %s: %v \n", msg.TopicPartition, string(msg.Value))
+					log.Infof("Message on %s: %v \n", msg.TopicPartition, string(msg.Value))
 					err = CreateEnrollment(enrollment, 1)
 					// Below condition flow will be used by WALK_IN component.
 					if err == nil {
 						// Push to ack topic
+						err = NotifyRecipient(enrollment)
+						if err != nil {
+							log.Error("Unable to send notification to the enrolled user",  err)
+						}
 					} else {
 						// Push to error topic
 					}
 					_, _ = consumer.CommitMessage(msg)
 				} else {
-					logrus.Info("Unable to serialize the request body", err)
+					log.Info("Unable to serialize the request body", err)
 				}
 
 			} else {
 				// The client will automatically try to recover from all errors.
-				logrus.Infof("Consumer error: %v \n", err)
+				log.Infof("Consumer error: %v \n", err)
 			}
 
 		}
