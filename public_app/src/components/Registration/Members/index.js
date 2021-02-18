56a59799c55cd3b1eff67f2a421c7ce0094fabc6
Samarthg <samarthg@sahajsoft.com>
2021-02-18 19:19:22 +0530

    integrating registration API with UI

--- public_app/src/components/Registration/Members/index.js
+++ public_app/src/components/Registration/Members/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import {useHistory} from "react-router-dom";
 import {CustomButton} from "../../CustomButton";
 import {useKeycloak} from "@react-keycloak/web";
-import {PROGRAM_API, RECIPIENT_CLIENT_ID, RECIPIENT_ROLE, SIDE_EFFECTS_DATA} from "../../../constants";
+import {PROGRAM_API, RECIPIENT_CLIENT_ID, RECIPIENT_ROLE, RECIPIENTS_API, SIDE_EFFECTS_DATA} from "../../../constants";
 import axios from "axios";
 import Card from "react-bootstrap/Card";
 
@@ -18,17 +18,17 @@ export const Members = () => {
         const mockMembers = [
             {
                 "name": "Rajesh Ravi",
-                "enrollmentId": "001304100",
+                "code": "001304100",
                 "programId": "1-7875daad-7ceb-4368-9a4b-7997e3b5b008"
             },
             {
                 "name": "Asha Ravi",
-                "enrollmentId": "001304102",
+                "code": "001304102",
                 "programId": "1-7875daad-7ceb-4368-9a4b-7997e3b5b008"
             }
         ];
         axios
-            .get("/divoc/api/v1/registrations")
+            .get(RECIPIENTS_API)
             .then((res) => {
                 if (res.status === '200') {
                     setMembers(res.body)
@@ -70,7 +70,7 @@ export const Members = () => {
                 "description":"This is the Phase 3 of the vaccination drive happening in the country. Eligible beneficiaries will have to register themselves on the citizen portal. Based on the enrolment code and the ID proof, beneficiaries will be vaccinated and issued a digital certificate that can be downloaded from the citizen portal.",
                 "endDate":"2021-06-30",
                 "medicineIds":["1-b6ebbbe4-b09e-45c8-b7a3-38828092da1a","1-2a62ae65-1ea5-4a23-946b-062fe5f512f6","1-9ac9eaf1-82bf-4135-b6ee-a948ae972fd4"],
-                "name":"C19 Vaccination Program",
+                "name":"Polio Vaccination",
                 "osCreatedAt":"2021-02-16T09:57:37.474Z",
                 "osUpdatedAt":"2021-02-17T09:37:23.195Z",
                 "osid":"1-7875daad-7ceb-4368-9a4b-7997e3b5b008",
@@ -101,7 +101,7 @@ export const Members = () => {
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
                     <Card.Title>{member.name}</Card.Title>
-                    <Card.Subtitle>Enrollment number: {member.enrollmentId}</Card.Subtitle>
+                    <Card.Subtitle>Enrollment number: {member.code}</Card.Subtitle>
                     <Card.Text>
                         {program ? program.name: ''}
                     </Card.Text>
--- public_app/src/constants.js
+++ public_app/src/constants.js
@@ -6,6 +6,7 @@ export const SIDE_EFFECTS_DATA = "SIDE_EFFECTS_DATA";
 export const API_ROOT_URL = 'https://api.covid19india.org/v4';
 export const CERTIFICATE_FILE = "certificate.json";
 export const PROGRAM_API = "/divoc/admin/api/v1/programs";
+export const RECIPIENTS_API = "/divoc/api/citizen/recipients";
 export const STATE_NAMES = {
   AP: 'Andhra Pradesh',
   AR: 'Arunachal Pradesh',
