6426afadb5c436250a239338e1ef6cce58bb3650
Tejash <tejash.jl1020@gmail.com>
2021-02-19 10:22:34 +0530

    Revert "replaced location.href with push"
    This reverts commit f9fd7383

--- public_app/src/CitizenLogin/citizen-login.js
+++ public_app/src/CitizenLogin/citizen-login.js
@@ -63,7 +63,7 @@ export function CitizenLoginComponent() {
         axios.post(url, {phone: state.phoneNumber, otp: state.otp})
             .then((response) => {
                 setCookie(CITIZEN_TOKEN_COOKIE_NAME, response.data.token, 1)
-                history.push("/registration")
+                window.location.href = "/registration";
             }).catch((error) => {
             setState((prevState) => {
                 return {
