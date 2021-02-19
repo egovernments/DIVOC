ef72dff53fb70d88f1efe0547b6d2f473d2fd402
Tejash <tejash.jl1020@gmail.com>
2021-02-19 15:39:02 +0530

    added age restrictions config

--- public_app/src/components/CustomDateWidget/index.js
+++ public_app/src/components/CustomDateWidget/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import date from "../../assets/img/date.png";
@@ -6,34 +6,34 @@ import "./index.css";
 
 
 export const CustomDateWidget = (props) => {
-  const [startDate, setStartDate] = useState(new Date(props.value || new Date()));
+    const [startDate, setStartDate] = useState(new Date(props.value || new Date()));
 
-  const updateValue = (newValue) => {
-    setStartDate(newValue);
-    props.onChange(newValue.toISOString().substring(0, 10))
-};
+    const updateValue = (newValue) => {
+        setStartDate(newValue);
+        props.onChange(newValue.toISOString().substring(0, 10))
+    };
 
-  const CustomInput = ({ value, onClick }) => (
-    <div className="date-picker-container d-flex justify-content-between">
-        <label className="date-picker-label p-2 mr-auto" onClick={onClick}>
-        {value}
-        </label>
-        <img className="p-2" src={date} onClick={onClick} />
-    </div>
-  );
-  return (
-    <div className="date-picker">
-    <DatePicker
-            selected={startDate} 
-            onChange={updateValue}
-            dateFormat="dd-MM-yyyy"
-            id="date-picker"
-            customInput={<CustomInput />}
-            maxDate={new Date()}
-            showYearDropdown
-        />
-    </div>
-  );
+    const CustomInput = ({value, onClick}) => (
+        <div className="date-picker-container d-flex justify-content-between">
+            <label className="date-picker-label p-2 mr-auto" onClick={onClick}>
+                {value}
+            </label>
+            <img className="p-2" src={date} onClick={onClick}/>
+        </div>
+    );
+    return (
+        <div className="date-picker">
+            <DatePicker
+                selected={startDate}
+                onChange={updateValue}
+                dateFormat="dd-MM-yyyy"
+                id="date-picker"
+                customInput={<CustomInput/>}
+                maxDate={props.maxDate}
+                showYearDropdown
+            />
+        </div>
+    );
 };
 
 
