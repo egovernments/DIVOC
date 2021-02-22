8dd1ec718a0184a5372550361687faef1e3820fe
Sai Sathvik Yepuri <saisathviky@sahaj.ai>
2021-02-22 17:20:24 +0530

    wait

--- e2e/e2e_test.py
+++ e2e/e2e_test.py
@@ -37,7 +37,7 @@ def test_certify():
     log.info("Cerify request sent")
     
     new_certs = []
-    max_tries = 5
+    max_tries = 12
     for i in range(max_tries):
         log.info("Fetching certificates...., try no : %s", i+1)
         new_certs = test_utils.fetch_certificates(GET_CERTIFICATE_BODY)
@@ -55,7 +55,7 @@ def run_tests():
     test_certify()
 
 tests_ran = False
-ping_retries = 10
+ping_retries = 24
 for i in range(ping_retries):
     log.info("Trying to ping...., try no : %s", i+1)
     if(service_check()):
