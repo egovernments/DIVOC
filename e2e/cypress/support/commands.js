c7cc311dbfc87a7005e9c07458f4a5781477e34b
burhanuddinrashid <burhanrashid5253@gmail.com>
2021-03-08 15:29:22 +0530

    Extracted common token and form request method into command.js

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -1,41 +1,67 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-// Performs an XMLHttpRequest instead of a cy.request (able to send data as
-// FormData - multipart/form-data)
-Cypress.Commands.add('form_request', (method, url, token, formData, done) => {
-    fetch(url, {
-        method: method,
+Cypress.Commands.add('divocFormRequest', (method, url, fileName) => {
+    return cy.fixture(fileName, 'binary')
+        .then(Cypress.Blob.binaryStringToBlob)
+        .then((blob) => {
+            return cy.divocGetToken()
+                .then(accessToken => {
+                    return {accessToken: accessToken, blob: blob}
+                })
+        }).then((result) => {
+            // Build up the form
+            const formData = new FormData();
+            formData.set('file', result.blob, fileName); //adding a file to the form
+            // Perform the request
+            return fetch(url, {
+                method: method,
+                headers: {
+                    "Authorization": `Bearer ${result.accessToken}`,
+                },
+                body: formData,
+            });
+        });
+})
+
+
+Cypress.Commands.add('divocRequest', (method, url, body) => {
+    return cy.divocGetToken()
+        .then((token) => {
+            return cy.request({
+                url: url,
+                method: method,
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                },
+                body: body,
+            });
+        });
+})
+
+
+const KEYCLOAK_URL = "http://localhost:8080/auth"
+const TOKEN_URL = KEYCLOAK_URL + "/realms/divoc/protocol/openid-connect/token"
+const ADMIN_API_CLIENT_SECRET = "8eb72e25-baff-4ec5-b636-d4c1b55257b5"
+
+const authBody = {
+    "grant_type": "client_credentials",
+    "client_id": "admin-api",
+    "client_secret": ADMIN_API_CLIENT_SECRET
+}
+
+Cypress.Commands.add('divocGetToken', () => {
+    return cy.request({
+        method: 'POST',
+        url: TOKEN_URL,
         headers: {
-            "Authorization": `Bearer ${token}`,
+            "Content-type": "application/x-www-form-urlencoded"
         },
-        body: formData,
-    }).then(r => {
-        done(r)
-    }).catch((e) => {
-        done(e)
-    })
+        body: authBody
+    }).then((result) => {
+        return result.body["access_token"]
+    });
 })
