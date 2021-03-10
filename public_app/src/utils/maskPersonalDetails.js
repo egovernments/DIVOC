5d4102331085df3eabc9bd323159c7a6fe3500e9
kesavanpalani <kesavanpalanipk@gmail.com>
2021-03-10 16:14:18 +0530

    Fix email masking issue

--- public_app/src/utils/maskPersonalDetails.js
+++ public_app/src/utils/maskPersonalDetails.js
@@ -2,7 +2,15 @@ export function maskPersonalDetails(personalDetail, maskAll=false){
     if (maskAll) {
         return personalDetail.replace(/^(.*)$/,(_, b) => b.replace(/./g, '*'))
     }
-
+    if(personalDetail && personalDetail.includes("@")){
+       return personalDetail.replace(/^(.)(.*)(@.*)$/,(_, a, b, c) => {
+           if(a.length === 1) {
+               return a.replace(/./g, '*') + b.replace(/./g, '*') + c
+           } else {
+               return a + b.replace(/./g, '*') + c
+           }
+       });
+    }
     if(personalDetail){
         const length = personalDetail.length * 0.6;
         const firstFragment = personalDetail.substring(0, length);
