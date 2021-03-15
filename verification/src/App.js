fc1d471a718cd2249bf21053e913bd12100aff31
Dileep Bapat <dileepbapat@gmail.com>
2021-03-15 10:40:44 +0530

    Adding verification application

---
+++ verification/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {VerifyCertificate} from "./components/VerifyCertificate";
+
+function App() {
+  return (
+    <div className="App">
+        <VerifyCertificate />
+    </div>
+  );
+}
+
+export default App;
---
+++ verification/src/constants.js
@@ -0,0 +1,53 @@
+const monthNames = [
+    "Jan", "Feb", "Mar", "Apr",
+    "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec"
+];
+
+export function formatDate(givenDate) {
+    const dob = new Date(givenDate);
+    let day = dob.getDate();
+    let monthName = monthNames[dob.getMonth()];
+    let year = dob.getFullYear();
+
+    return `${day}-${monthName}-${year}`;
+}
+
+export const CertificateDetailsPaths = {
+    "Name": {
+        path: ["credentialSubject", "name"],
+        format: (data) => (data)
+    },
+    "Age": {
+        path: ["credentialSubject", "age"],
+        format: (data) => (data)
+    },
+    "Gender": {
+        path: ["credentialSubject", "gender"],
+        format: (data) => (data)
+    },
+    "Certificate ID": {
+        path: ["evidence", "0", "certificateId"],
+        format: (data) => (data)
+    },
+    "Date of Issue": {
+        path: ["evidence", "0", "effectiveStart"],
+        format: (data) => (formatDate(data))
+    },
+    "Valid Until": {
+        path: ["evidence", "0", "effectiveUntil"],
+        format: (data) => (formatDate(data))
+    },
+    "Dose": {
+        path: ["evidence", "0", "dose"],
+        format: (data) => (data)
+    },
+    "Total Doses": {
+        path: ["evidence", "0", "totalDoses"],
+        format: (data) => (data)
+    },
+    "Vaccination Facility": {
+        path: ["evidence", "0", "facility", "name"],
+        format: (data) => (data)
+    }
+};
