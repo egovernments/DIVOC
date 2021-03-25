366a6b791348fda04d80bc586d6634b674a1f611
Tejash <tejash.jl1020@gmail.com>
2021-03-25 15:48:00 +0530

    updated to skip otp form for offline scenario

--- mobile/src/components/VerifyMobile/index.js
+++ mobile/src/components/VerifyMobile/index.js
@@ -3,10 +3,12 @@ import {BaseFormCard} from "../BaseFormCard";
 import {getMessageComponent, LANGUAGE_KEYS} from "../../lang/LocaleContext";
 import "./index.css";
 import {CustomButton} from "../CustomButton";
-import {useWalkInEnrollment} from "../WalkEnrollments/context";
+import {FORM_WALK_IN_ENROLL_FORM, useWalkInEnrollment} from "../WalkEnrollments/context";
 import {FORM_WALK_IN_VERIFY_MOBILE, FORM_WALK_IN_VERIFY_OTP} from "../WalkEnrollments/context";
+import {useOnlineStatus} from "../../utils/offlineStatus";
 
 export const VerifyMobile = () => {
+    const isOnline = useOnlineStatus();
     const [phone, setPhone] = useState("");
     const [errors, setErrors] = useState({});
     const {goNext} = useWalkInEnrollment();
@@ -19,18 +21,25 @@ export const VerifyMobile = () => {
         }
     }
 
+    function goToForm() {
+        goNext(FORM_WALK_IN_VERIFY_OTP, FORM_WALK_IN_ENROLL_FORM, {})
+    }
+
     return (
         <div className="new-enroll-container">
             <BaseFormCard title={getMessageComponent(LANGUAGE_KEYS.ENROLLMENT_TITLE)}>
                 <div className="verify-mobile-container">
-                    <h5>Verify Mobile</h5>
+                    <h5>{isOnline ? 'Verify Mobile' : 'Enter Mobile Number'}</h5>
                     <input className="w-100 mt-5 mb-3 mobile-input" type="tel" value={phone}
                            onChange={(evt) => setPhone(evt.target.value)} autoFocus={true}
                            placeholder="Enter mobile number" maxLength={10}/>
                     <div className="invalid-input m-0 text-left">
                         {errors.mobile}
                     </div>
-                    <CustomButton className="primary-btn w-100" onClick={onGetOTP}>GET OTP</CustomButton>
+                    {
+                        isOnline ? <CustomButton className="primary-btn w-100" onClick={onGetOTP}>GET OTP</CustomButton> :
+                            <CustomButton className="primary-btn w-100" onClick={goToForm}>CONTINUE</CustomButton>
+                    }
                 </div>
             </BaseFormCard>
         </div>
