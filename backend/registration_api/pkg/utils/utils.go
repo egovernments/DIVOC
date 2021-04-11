f55f25a830d3bd715d9030fa3bd7f0673d1492f3
Tejash <tejash.jl1020@gmail.com>
2021-04-11 13:11:55 +0530

    updated dose value to string

--- backend/registration_api/pkg/utils/utils.go
+++ backend/registration_api/pkg/utils/utils.go
@@ -83,3 +83,26 @@ func Filter(entries []string, criteria func(s string) bool) []string {
 	}
 	return res
 }
+
+func ToString(arg interface{}) string {
+	switch v := arg.(type) {
+	case int:
+		return strconv.Itoa(v)
+	case int8:
+		return strconv.FormatInt(int64(v), 10)
+	case int16:
+		return strconv.FormatInt(int64(v), 10)
+	case int32:
+		return strconv.FormatInt(int64(v), 10)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case string:
+		return v
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	default:
+		return ""
+	}
+}
