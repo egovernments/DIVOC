5c1660d780ccd3923a15c8be2bb11d039d4e3930
Samarth G R <samarthgiripura@gmail.com>
2021-08-12 20:20:21 +0530

    multilingual support for registration flow

--- public_app/src/components/Registration/AddMember/Success.js
+++ public_app/src/components/Registration/AddMember/Success.js
@@ -4,10 +4,12 @@ import {maskPersonalDetails} from "../../../utils/maskPersonalDetails";
 import {CustomConfirmPage} from "../../CustomConfirmPage";
 import {getNameOfTheId, getNationalIdType} from "../../../utils/national-id";
 import {Loader} from "../../Loader";
+import {useTranslation} from "react-i18next";
 
 export const Success = ({formData, programs}) => {
     const history = useHistory();
     const [isLoading, setIsLoading] = useState(false);
+    const {t} = useTranslation();
 
     let programName = programs.filter(p => p.osid === formData.programId)[0].name
     return (
@@ -19,18 +21,21 @@ export const Success = ({formData, programs}) => {
                     history.push("/registration")
                 }, 5000)
             }}>
-                <h3>Successfully registered for {programName ? programName : ''}</h3>
+                <h3>{t('registration.success.title', {programName: programName ? programName : ''})}</h3>
                 <div className="pt-3">
-                    <h4>Beneficiary Name: {formData.name}</h4>
+                    <h4>{t('registration.beneficiaryName')}: {formData.name}</h4>
                 </div>
                 <div className="pt-3">
-                    <p>Enrolment details will be sent to <br/>
+                    <p>
+                        {t('registration.success.enrollmentDetails')}<br/>
                         {formData.email ? maskPersonalDetails(formData.email).concat(" and ") : ''}
-                        {maskPersonalDetails(formData.contact)}</p>
+                        {maskPersonalDetails(formData.contact)}
+                    </p>
                 </div>
                 <div className="pt-3">
-                    <p>On the day of vaccination, please carry your
-                        original {getNameOfTheId(getNationalIdType(formData.identity))} for verification</p>
+                    <p>
+                        {t('registration.success.vaccinationInfo', {identity: getNameOfTheId(getNationalIdType(formData.identity))})}
+                    </p>
                 </div>
             </CustomConfirmPage>
         </>
