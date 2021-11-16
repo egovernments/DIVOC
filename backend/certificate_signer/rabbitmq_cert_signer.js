ed5b5f56f1661796ca7bf4803b99f9261ca878db
himeshr <72493481+himeshr@users.noreply.github.com>
2021-11-16 17:48:16 +0530

    Update kafka_cert_signer and rabbitmq_cert_signer to make use of  signer.init_signer() and signer.signCertificate()

--- backend/certificate_signer/rabbitmq_cert_signer.js
+++ backend/certificate_signer/rabbitmq_cert_signer.js
@@ -1,28 +1,66 @@
-# Access the callback-based API
+const {
+  CERTIFICATE_NAMESPACE,
+  CERTIFICATE_NAMESPACE_V2,
+  CERTIFICATE_ISSUER,
+  CERTIFICATE_BASE_URL,
+  CERTIFICATE_FEEDBACK_BASE_URL,
+  CERTIFICATE_INFO_BASE_URL,
+  ENABLE_FEEDBACK_URL
+} = require ("./config/config");
 const amqp = require('amqplib/callback_api');
 const config = require('./config/config');
-const signer = require('./signer');
-const {publicKeyPem, privateKeyPem} = require('./config/keys');
-const redis = require('./redis');
+const constants = require("./config/constants");
 const R = require('ramda');
-const {RABBITMQ_SERVER} = require('../../configs/config');
-const CERTIFY_TOPIC = config.CERTIFY_TOPIC;
-const CERTIFIED_TOPIC = config.CERTIFIED_TOPIC;
-const DUPLICATE_CERTIFICATE_TOPIC = config.DUPLICATE_CERTIFICATE_TOPIC
-const ERROR_CERTIFICATE_TOPIC = config.ERROR_CERTIFICATE_TOPIC;
-const CERTIFICATE_ACK_TOPIC = 'certify_ack';
-const INPROGRESS_KEY_EXPIRY_SECS = 5 * 60;
-const CERTIFICATE_INPROGRESS = "P";
-const REGISTRY_SUCCESS_STATUS = "SUCCESSFUL";
-const REGISTRY_FAILED_STATUS = "UNSUCCESSFUL";
+const {vaccinationContext, vaccinationContextV2} = require("vaccination-context");
+const signer = require('certificate-signer-library');
+const {publicKeyPem, privateKeyPem} = require('./config/keys');
+
+const CERTIFICATE_TYPE_V2 = "certifyV2";
+const CERTIFICATE_TYPE_V3 = "certifyV3";
+
 const PREFETCH_MSGS_VALUE = 1;
 const DEFAULT_ROUTING_KEY = "";
 
 var isConnecting = false;
+
 var amqpConn = null;
 var pubChannel = null;
 
-function initRabbitmqCertSigner() {
+let signingConfig = {
+  publicKeyPem: publicKeyPem,
+  privateKeyPem: privateKeyPem,
+
+  RABBITMQ_SERVER: config.RABBITMQ_SERVER,
+
+  REGISTRY_URL: config.REGISTRY_URL,
+  REGISTRY_CERTIFICATE_SCHEMA: config.REGISTRY_CERTIFICATE_SCHEMA,
+  REDIS_URL: config.REDIS_URL,
+  REDIS_KEY_EXPIRE: config.REDIS_KEY_EXPIRE,
+
+  CERTIFICATE_NAMESPACE: config.CERTIFICATE_NAMESPACE,
+  CERTIFICATE_CONTROLLER_ID: config.CERTIFICATE_CONTROLLER_ID,
+  CERTIFICATE_DID: config.CERTIFICATE_DID,
+  CERTIFICATE_PUBKEY_ID: config.CERTIFICATE_PUBKEY_ID,
+  CERTIFICATE_ISSUER: config.CERTIFICATE_ISSUER,
+  CERTIFICATE_BASE_URL: config.CERTIFICATE_BASE_URL,
+  CERTIFICATE_FEEDBACK_BASE_URL: config.CERTIFICATE_FEEDBACK_BASE_URL,
+  CERTIFICATE_INFO_BASE_URL: config.CERTIFICATE_INFO_BASE_URL,
+
+  CERTIFY_TOPIC: config.CERTIFY_TOPIC,
+  CERTIFIED_TOPIC: config.CERTIFIED_TOPIC,
+  ENABLE_CERTIFY_ACKNOWLEDGEMENT: config.ENABLE_CERTIFY_ACKNOWLEDGEMENT,
+  ERROR_CERTIFICATE_TOPIC: config.ERROR_CERTIFICATE_TOPIC,
+  CERTIFICATE_RETRY_COUNT: config.CERTIFICATE_RETRY_COUNT,
+  DUPLICATE_CERTIFICATE_TOPIC: config.DUPLICATE_CERTIFICATE_TOPIC,
+  CERTIFICATE_ACK_TOPIC: config.CERTIFICATE_ACK_TOPIC,
+
+};
+
+const documentLoader = {};
+documentLoader[CERTIFICATE_NAMESPACE] = vaccinationContext;
+documentLoader[CERTIFICATE_NAMESPACE_V2] = vaccinationContextV2;
+
+async function initRabbitmqCertSigner() {
   console.log('Using ' + RABBITMQ_SERVER);
   console.log('Using ' + publicKeyPem);
   if (isConnecting)
@@ -50,6 +88,7 @@ function initRabbitmqCertSigner() {
 }
 
 function whenConnected() {
+  await signer.init_signer(signingConfig, transformW3, documentLoader);
   startPublisher();
   startConsumer();
 }
@@ -80,25 +119,6 @@ function publish(exchange, routingKey, content) {
          });
   } catch (e) {
     console.error("[AMQP] publish", e.message);
-  }
-}
-
-function sendCertAckEvents(exchange, routingKey, uploadId, rowId, status, errMsg="") {
-  var msg = [{key: null, value: JSON.stringify({
-    uploadId: uploadId,
-    rowId: rowId,
-    status: status,
-    errorMsg: errMsg})}];
-  publish(exchange, routingKey, Buffer.from(msg));
-}
-
-async function sendCertifyAck(status, uploadId, rowId, errMsg="") {
-  if (config.ENABLE_CERTIFY_ACKNOWLEDGEMENT) {
-    if (status === REGISTRY_SUCCESS_STATUS) {
-      sendCertAckEvents(CERTIFICATE_ACK_TOPIC, DEFAULT_ROUTING_KEY, uploadId, rowId, 'SUCCESS', '');
-    } else if (status === REGISTRY_FAILED_STATUS) {
-      sendCertAckEvents(CERTIFICATE_ACK_TOPIC, DEFAULT_ROUTING_KEY, uploadId, rowId, 'FAILED', errMsg);
-    }
   }
 }
 
@@ -154,36 +174,7 @@ async function signCert(message, cb) {
       throw Error("Required parameters not available")
     }
     const key = `${preEnrollmentCode}-${programId}-${currentDose}`;
-    const isSigned = await redis.checkIfKeyExists(key);
-    const isUpdateRequest = R.pathOr(false, ["meta", "previousCertificateId"], jsonMessage);
-    if (!isSigned || isUpdateRequest) {
-      redis.storeKeyWithExpiry(key, CERTIFICATE_INPROGRESS, INPROGRESS_KEY_EXPIRY_SECS);
-      await signer.signAndSave(jsonMessage)
-        .then(res => {
-          console.log(`${preEnrollmentCode} | statusCode: ${res.status} `);
-          if (process.env.DEBUG) {
-             console.log(res);
-          }
-          let errMsg;
-          if (res.status === 200) {
-            sendCertifyAck(res.data.params.status, uploadId, rowId, res.data.params.errmsg);
-            var msg = {key: null, value: JSON.stringify(res.signedCertificate)};
-            publish(CERTIFIED_TOPIC, DEFAULT_ROUTING_KEY, Buffer.from(msg));
-          } else {
-            errMsg = "error occurred while signing/saving of certificate - " + res.status;
-            sendCertifyAck(REGISTRY_FAILED_STATUS, uploadId, rowId, errMsg)
-          }
-        })
-        .catch(error => {
-          console.error(error)
-          sendCertifyAck(REGISTRY_FAILED_STATUS, uploadId, rowId, error.message)
-          throw error
-        });
-    } else {
-      console.error("Duplicate pre-enrollment code received for certification :" + preEnrollmentCode)
-      var msg = {key: null, value: JSON.stringify({message: message.value.toString(), error: "Duplicate pre-enrollment code"})};
-      await publish(DUPLICATE_CERTIFICATE_TOPIC, DEFAULT_ROUTING_KEY, Buffer.from(msg));
-    }
+    await signer.signCertificate(jsonMessage, message.headers, key);
   } catch (e) {
     // const preEnrollmentCode = R.pathOr("", ["preEnrollmentCode"], jsonMessage);
     // const currentDose = R.pathOr("", ["vaccination", "dose"], jsonMessage);
@@ -192,7 +183,7 @@ async function signCert(message, cb) {
     // }
     console.error("ERROR: " + e.message)
     var msg = {key: null, value: JSON.stringify({message: message.value.toString(), error: e.message})};
-    await publish(ERROR_CERTIFICATE_TOPIC, DEFAULT_ROUTING_KEY, Buffer.from(msg));
+    publish(ERROR_CERTIFICATE_TOPIC, DEFAULT_ROUTING_KEY, Buffer.from(msg));
   }
   console.timeEnd("certify");
   cb(true);
@@ -203,6 +194,118 @@ function closeOnErr(err) {
   console.error("[AMQP] error", err);
   amqpConn.close();
   return true;
+}
+
+function ageOfRecipient(recipient) {
+  if (recipient.age) return recipient.age;
+  if (recipient.dob && new Date(recipient.dob).getFullYear() > 1900)
+    return "" + (Math.floor((new Date() - new Date(recipient.dob))/1000/60/60/24.0/365.25));
+  return "";
+}
+
+function dobOfRecipient(recipient) {
+  if (recipient.dob && new Date(recipient.dob).getFullYear() > 1900) return recipient.dob;
+  // administrative dob
+  if (recipient.age && recipient.age > 0)
+    return (new Date().getFullYear() - recipient.age) + "-01-01";
+  return "";
+}
+
+function transformW3(cert, certificateId) {
+  const certificateType = R.pathOr('', ['meta', 'certificateType'], cert)
+
+  let certificateFromTemplate = {
+    "@context": [
+      "https://www.w3.org/2018/credentials/v1",
+      CERTIFICATE_NAMESPACE,
+    ],
+    type: ['VerifiableCredential', 'ProofOfVaccinationCredential'],
+    credentialSubject: {
+      type: "Person",
+      id: R.pathOr('', ['recipient', 'identity'], cert),
+      refId: R.pathOr('', ['preEnrollmentCode'], cert),
+      name: R.pathOr('', ['recipient', 'name'], cert),
+      gender: R.pathOr('', ['recipient', 'gender'], cert),
+      age: ageOfRecipient(cert.recipient), //from dob
+      nationality: R.pathOr('', ['recipient', 'nationality'], cert),
+      address: {
+        "streetAddress": R.pathOr('', ['recipient', 'address', 'addressLine1'], cert),
+        "streetAddress2": R.pathOr('', ['recipient', 'address', 'addressLine2'], cert),
+        "district": R.pathOr('', ['recipient', 'address', 'district'], cert),
+        "city": R.pathOr('', ['recipient', 'address', 'city'], cert),
+        "addressRegion": R.pathOr('', ['recipient', 'address', 'state'], cert),
+        "addressCountry": R.pathOr('IND', ['recipient', 'address', 'country'], cert),
+        "postalCode": R.pathOr('', ['recipient', 'address', 'pincode'], cert),
+      }
+    },
+    issuer: CERTIFICATE_ISSUER,
+    issuanceDate: new Date().toISOString(),
+    evidence: [{
+      "id": CERTIFICATE_BASE_URL + certificateId,
+      "infoUrl": CERTIFICATE_INFO_BASE_URL + certificateId,
+      "certificateId": certificateId,
+      "type": ["Vaccination"],
+      "batch": R.pathOr('', ['vaccination', 'batch'], cert),
+      "vaccine": R.pathOr('', ['vaccination', 'name'], cert),
+      "manufacturer": R.pathOr('', ['vaccination', 'manufacturer'], cert),
+      "date": R.pathOr('', ['vaccination', 'date'], cert),
+      "effectiveStart": R.pathOr('', ['vaccination', 'effectiveStart'], cert),
+      "effectiveUntil": R.pathOr('', ['vaccination', 'effectiveUntil'], cert),
+      "dose": R.pathOr('', ['vaccination', 'dose'], cert),
+      "totalDoses": R.pathOr('', ['vaccination', 'totalDoses'], cert),
+      "verifier": {
+        "name": R.pathOr('', ['vaccinator', 'name'], cert),
+      },
+      "facility": {
+        // "id": CERTIFICATE_BASE_URL + cert.facility.id,
+        "name": R.pathOr('', ['facility', 'name'], cert),
+        "address": {
+          "streetAddress": R.pathOr('', ['facility', 'address', 'addressLine1'], cert),
+          "streetAddress2": R.pathOr('', ['facility', 'address', 'addressLine2'], cert),
+          "district": R.pathOr('', ['facility', 'address', 'district'], cert),
+          "city": R.pathOr('', ['facility', 'address', 'city'], cert),
+          "addressRegion": R.pathOr('', ['facility', 'address', 'state'], cert),
+          "addressCountry": R.pathOr('IND', ['facility', 'address', 'country'], cert),
+          "postalCode": R.pathOr('', ['facility', 'address', 'pincode'], cert)
+        },
+      }
+    }],
+    "nonTransferable": "true"
+  };
+
+  if (ENABLE_FEEDBACK_URL) {
+    certificateFromTemplate["evidence"][0] = {
+      ...certificateFromTemplate["evidence"][0],
+      "feedbackUrl": CERTIFICATE_FEEDBACK_BASE_URL + certificateId
+    };
+
+  }
+
+  if (certificateType === CERTIFICATE_TYPE_V3) {
+    const vaccineName = R.pathOr('', ['vaccination', 'name'], cert);
+    const icd11Code = vaccineName ? constants.VACCINE_ICD11_MAPPINGS.filter(a => vaccineName.toLowerCase().includes(a.vaccineName)).map(a => a.icd11Code)[0]: '';
+    const prophylaxis = icd11Code ? constants.ICD11_MAPPINGS[icd11Code]["icd11Term"]: '';
+    // update context
+    certificateFromTemplate["@context"] = [
+      "https://www.w3.org/2018/credentials/v1",
+      "https://cowin.gov.in/credentials/vaccination/v2"
+    ];
+
+    // dob
+    certificateFromTemplate["credentialSubject"] = {
+      ...certificateFromTemplate["credentialSubject"],
+      "dob": dobOfRecipient(cert.recipient),
+    };
+
+    // icd11code
+    certificateFromTemplate["evidence"][0] = {
+      ...certificateFromTemplate["evidence"][0],
+      "icd11Code": icd11Code ? icd11Code: '',
+      "prophylaxis": prophylaxis ? prophylaxis: '',
+    };
+
+  }
+  return certificateFromTemplate;
 }
 
 module.exports = {
