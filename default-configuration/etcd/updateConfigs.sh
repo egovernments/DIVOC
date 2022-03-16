3e4a9e2af69146cb45fe1e88fe41e00416f17337
ajay-beehyv <ajaykumar.y@beehyv.com>
2022-03-16 16:02:33 +0530

    minor refactoring

--- default-configuration/etcd/updateConfigs.sh
+++ default-configuration/etcd/updateConfigs.sh
@@ -3,7 +3,6 @@
 export ETCDCTL_API=3
 ETCD_KEY_VALUE_FILES=(DDCC_TEMPLATE.template W3C_TEMPLATE.template euVaccineCode.json euVaccineManuf.json ICD.json VACCINE_ICD.json euVaccineProph.json certificateOptionalFieldsKeyPaths.json testCertificateTemplate.html vaccineCertificateTemplate.html certificateHelperFunctions.js)
 for FILE in ${ETCD_KEY_VALUE_FILES[@]}; do
-echo $FILE
 Arr=($(echo $FILE | tr "." "\n"))
 KEY=${Arr[0]}
 cat $FILE | etcdctl put -- $KEY
