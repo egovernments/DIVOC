f8713bcc9780c8bc2bd7ce51668f574a62628403
Vishwajeet <chauhan.vishwajeet24@gmail.com>
2022-04-21 12:23:29 +0530

    keycloak update 16.0.1

--- keycloak-mobile-number-login-spi/build.sh
+++ keycloak-mobile-number-login-spi/build.sh
@@ -2,3 +2,5 @@
 mkdir -p ../keycloak/providers && cp target/keycloak-mobile-number-login-spi-1.0-SNAPSHOT.jar ../keycloak/providers
 cp -r themes/sys-admin ../keycloak/themes/
 cp -r themes/facility-operator ../keycloak/themes/
+cp -r themes/facility-admin ../keycloak/themes/
+cp -r themes/certificate-login ../keycloak/themes/
