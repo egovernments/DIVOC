27f6f45538e527736640277c7463465e50855b40
ajay-beehyv <ajaykumar.y@beehyv.com>
2022-06-15 16:41:15 +0530

    remove public_app, portal, facility_app, verification_app

--- mobile/README.md
+++
@@ -1,1 +0,0 @@
-# Vaccination React Application (PWA)
--- portal/src/components/About.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-    return (
-        <div>
-            <h2>About</h2>
-            <p>DIVOC is digital infrastructure for vaccination and open certification.</p>
-        </div>
-    );
-}
