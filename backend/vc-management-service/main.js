42e9f1b69c3261edd6802f1b51d7909d85c5257d
Tejas Varade <varadetejas@gmail.com>
2022-08-02 19:56:26 +0530

    HLM-473, 474 | Req and res logging in file using middleware

--- backend/vc-management-service/main.js
+++ backend/vc-management-service/main.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const yaml = require('yamljs')
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+
 const issuerConfig = require('./src/configs/config');
 const {BASE_URL} = require("./src/configs/config");
 let issuerRouter = require('./src/routes/issuer.routes');
@@ -14,6 +18,26 @@ const app = express();
 const port = issuerConfig.PORT;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use((bodyParser.json()));
+
+// var fileWrite = fs.createWriteStream(path.join(__dirname, 'logs.log'), {flags: 'a'});
+var fileWrite = rfs.createStream('reqres.log', {
+    interval: '1d',
+    path: path.join(__dirname, 'log')
+});
+app.use(morgan(function(tokens, req, res) {
+    return ["\n\n",
+        "Method : " + tokens.method(req, res),
+        "URL : " + tokens.url(req, res),
+        "Status : " + tokens.status(req, res),
+        "Headers : " + function(req, res) {
+            return JSON.stringify(req.headers);
+        }(req, res),
+        "Body : " + function(req, res) {
+            return JSON.stringify(req.body);
+        }(req, res),
+        "Date : " + new Date() 
+    ].join('\n')
+}, {stream: fileWrite}));
 
 app.use(`${BASE_URL}v1/issuer`, issuerRouter);
 app.use(`${BASE_URL}v1/schema`, schemaRouter);
