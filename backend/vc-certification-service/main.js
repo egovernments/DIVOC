7cf60209ac1e0cab088558bdfad22e0ecea429f4
Tejas Varade <varadetejas@gmail.com>
2022-08-04 16:42:47 +0530

    DIV-473, 474 | Log to console

--- backend/vc-certification-service/main.js
+++ backend/vc-certification-service/main.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
-const yaml = require('yamljs')
-const morgan = require('morgan');
-const path = require('path');
-const rfs = require('rotating-file-stream');
+const yaml = require('yamljs');
+const morganBody = require('morgan-body');
 
 const certifyConfig = require('./src/configs/config');
 const {BASE_URL} = require("./src/configs/config");
@@ -17,24 +15,10 @@ const port = certifyConfig.PORT;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use((bodyParser.json()));
 
-var fileWrite = rfs.createStream('reqres.log', {
-    interval: '1d',
-    path: path.join(__dirname, 'log')
+morganBody(app, {
+    dateTimeFormat: 'iso',
+    maxBodyLength: 1000000
 });
-app.use(morgan(function(tokens, req, res) {
-    return ["\n\n",
-        "Method : " + tokens.method(req, res),
-        "URL : " + tokens.url(req, res),
-        "Status : " + tokens.status(req, res),
-        "Headers : " + function(req, res) {
-            return JSON.stringify(req.headers);
-        }(req, res),
-        "Body : " + function(req, res) {
-            return JSON.stringify(req.body);
-        }(req, res),
-        "Date : " + new Date() 
-    ].join('\n')
-}, {stream: fileWrite}));
 
 app.use(`${BASE_URL}v1`, certifyRouter);
 
