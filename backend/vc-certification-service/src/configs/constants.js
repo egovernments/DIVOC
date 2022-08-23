19907d21e820a6d040b70b1ddafc025f700b3f03
Tejas Varade <varadetejas@gmail.com>
2022-08-23 20:56:44 +0530

    HLM-543 | Update default value for vc-certify kafka topic

--- backend/vc-certification-service/src/configs/constants.js
+++ backend/vc-certification-service/src/configs/constants.js
@@ -3,7 +3,7 @@ const config = require('./config');
 const SUNBIRD_CERTIFICATE_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/`;
 const SVG_ACCEPT_HEADER = "image/svg+xml";
 const IMAGE_RESPONSE_TYPE = 'svg';
-const VC_CERTIFY_TOPIC = process.env.VC_CERTIFY_TOPIC;
+const VC_CERTIFY_TOPIC = process.env.VC_CERTIFY_TOPIC || 'vc-certify';
 module.exports = {
     SUNBIRD_CERTIFICATE_URL,
     SVG_ACCEPT_HEADER,
