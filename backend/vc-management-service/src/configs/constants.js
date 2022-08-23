e1f7ac5a62e47f5515ac44b72ca4d2a068c247d1
bhanu <bhanu.prakash@egovernments.org>
2022-08-23 12:49:27 +0530

    HLM-539 | adding mandatory fields

--- backend/vc-management-service/src/configs/constants.js
+++ backend/vc-management-service/src/configs/constants.js
@@ -7,6 +7,8 @@ const SUNBIRD_TEMPLATE_UPLOAD_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/:issu
 const SUNBIRD_GET_SCHEMA_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/Schema/:schemaId`
 const MINIO_URL_SCHEME = "minio://"
 const SUNBIRD_GET_TRANSACTION_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/TransactionOsidMap/search`;
+const MANDATORY_FIELDS = ["issuer", "issuanceDate", "validFrom", "validUntil" , "certificateId"];
+const MANDATORY_FIELD_EVIDENCE_INDEX_START = 2 ;
 
 module.exports = {
     SUNBIRD_ISSUER_INVITE_URL,
@@ -15,5 +17,7 @@ module.exports = {
     SUNBIRD_SCHEMA_UPDATE_URL,
     SUNBIRD_GET_SCHEMA_URL,
     MINIO_URL_SCHEME,
-    SUNBIRD_GET_TRANSACTION_URL
+    SUNBIRD_GET_TRANSACTION_URL,
+    MANDATORY_FIELDS,
+    MANDATORY_FIELD_EVIDENCE_INDEX_START
 }
