06840c11fecdbe62cec00290cb4101d582f5dbd4
Vishwajeet <chauhan.vishwajeet24@gmail.com>
2022-08-25 14:21:45 +0530

    Not to be pushed

--- backend/vc-certification-service/src/configs/config.js
+++ backend/vc-certification-service/src/configs/config.js
@@ -1,6 +1,6 @@
-const SUNBIRD_REGISTRY_URL = process.env.SUNBIRD_REGISTRY_URL;
+const SUNBIRD_REGISTRY_URL = process.env.SUNBIRD_REGISTRY_URL||'http://localhost:8081';
 const PORT = process.env.PORT || 7654;
-const KEYCLOAK_URL = process.env.KEYCLOAK_URL;
+const KEYCLOAK_URL = process.env.KEYCLOAK_URL||'http://keycloak:8080';
 const KEYCLOAK_REALM = process.env.KEYCLOAK_REALM || 'sunbird-rc';
 const JWT_PUBLIC_KEY = process.env.AUTH_PUBLIC_KEY;
 const BASE_URL = process.env.ISSUER_SERVICE_BASE_URL || '/vc-certification/';
