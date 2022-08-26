9fb4a5c5c69c85d5f15cadeb4e4daf137b63c063
ajay-beehyv <ajaykumar.y@beehyv.com>
2022-08-26 15:43:29 +0530

    HLM-575 | remove manage, certification roles and code refactoring in vc-certify-consumer

--- backend/vc-management-service/src/middleware/auth.middleware.js
+++ backend/vc-management-service/src/middleware/auth.middleware.js
@@ -30,8 +30,7 @@ async function roleAuthorizer(req, res, next){
         const token = bearerToken.split(" ")[1];
         const decodedPayload = jwt.decode(token);
         const payloadRoles = decodedPayload.resource_access["admin-api"]?.roles;
-        const payloadVcRoles = decodedPayload.resource_access["manage"].roles;
-        if(payloadRoles.includes("api") && payloadVcRoles.includes("vc-manage")){
+        if(payloadRoles.includes("api")){
             next();
         }else{
             res.status(403).send({error: "role not authorized"});
