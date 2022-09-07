66f7a2253248b7982c38e3223260a2f89c5a5124
Kuldeep Garg <kldpgarg1@gmail.com>
2022-09-07 13:05:05 +0530

    change schedule to run on daily basis

--- backend/vc-certification-service/src/configs/constants.js
+++ backend/vc-certification-service/src/configs/constants.js
@@ -7,7 +7,7 @@ const VC_CERTIFY_TOPIC = process.env.VC_CERTIFY_TOPIC || 'vc-certify';
 const VC_REMOVE_SUSPENSION_TOPIC = process.env.VC_REMOVE_SUSPENSION_TOPIC || 'vc-remove-suspension';
 const SUNBIRD_SSO_CLIENT = "admin-api";
 const SUNBIRD_SSO_ADMIN_CLIENT_SECRET = "0358fa30-6014-4192-9551-7c61b15b774c";
-const SUSPENSION_REMOVAL_SCHEDULE = '* * * * *'; //actual value to be used - '0 0 * * *'
+const SUSPENSION_REMOVAL_SCHEDULE = '0 0 * * *';
 module.exports = {
     SUNBIRD_CERTIFICATE_URL,
     SVG_ACCEPT_HEADER,
