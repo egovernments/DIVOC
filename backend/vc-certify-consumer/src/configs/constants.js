11f011e054fda255171ef30b42f5f557d6653c84
Vishwajeet <chauhan.vishwajeet24@gmail.com>
2022-09-08 11:49:48 +0530

    HLM 572 | constants file updated

--- backend/vc-certify-consumer/src/configs/constants.js
+++ backend/vc-certify-consumer/src/configs/constants.js
@@ -2,7 +2,7 @@ const config = require('./config');
 
 const SUNBIRD_TRANSACTION_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/`;
 const TRANSACTION_ENTITY_TYPE = "TransactionCertificateMap";
-const VC_CERTIFICATION_SERVICE_URL = `${config.VC_CERTIFICATION_SERVICE_URL}/v1/certificate/revoke`;
+const VC_CERTIFICATION_SERVICE_URL = `${config.VC_CERTIFICATION_SERVICE_URL}/vc-certification/v1/certificate/revoke`;
 
 module.exports = {
     SUNBIRD_TRANSACTION_URL,
