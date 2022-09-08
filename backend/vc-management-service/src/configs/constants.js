1abd57276f9c95696a95efea81a2fa1c12d878f2
bhanu <bhanu.prakash@egovernments.org>
2022-09-08 19:25:09 +0530

    code-refactor

--- backend/vc-management-service/src/configs/constants.js
+++ backend/vc-management-service/src/configs/constants.js
@@ -1,5 +1,4 @@
 const config = require('./config');
-
 const SUNBIRD_TENANT_INVITE_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/Tenant/invite`;
 const SUNBIRD_SCHEMA_ADD_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/Schema`;
 const SUNBIRD_SCHEMA_UPDATE_URL = `${config.SUNBIRD_REGISTRY_URL}/api/v1/Schema/:schemaId`;
