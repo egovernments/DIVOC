25e24e19e86adae6ae20ccc01c765e597f31f5b1
bhanu <bhanu.prakash@egovernments.org>
2022-10-19 15:42:14 +0530

    adding gradient

--- tenant-portal/src/components/GenericButton/GenericButton.js
+++ tenant-portal/src/components/GenericButton/GenericButton.js
@@ -4,7 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const GenericButton = (props) => {
   return (
-        <Button variant='primary' className='m-1'><img src={props.img} alt=""/>{props.text}</Button>
+        <Button variant='primary' className='m-1' 
+        style={{ background: 'linear-gradient(to right, rgba(115,186,244,1), rgba(83,103,202,1))' }}>
+            <img src={props.img} alt="" className='me-3'/><strong>{props.text}</strong>
+        </Button>
   )
 }
 
