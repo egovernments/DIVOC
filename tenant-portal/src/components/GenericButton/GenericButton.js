025c7b23cb5d4f927492633b31d1a2e609c13ccd
bhanu <bhanu.prakash@egovernments.org>
2022-10-20 16:56:59 +0530

    className and styles from props

--- tenant-portal/src/components/GenericButton/GenericButton.js
+++ tenant-portal/src/components/GenericButton/GenericButton.js
@@ -4,10 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const GenericButton = (props) => {
   return (
-        <Button type={props.type} className='m-1' 
-        style={{ background: 'linear-gradient(to right, rgba(115,186,244,1), rgba(83,103,202,1))' }}>
-            <img src={props.img} alt="" className='me-3'/><strong>{props.text}</strong>
-        </Button>
+    <Button variant={props.type} className={props.className} style={props.styles}>
+        <img src={props.img} alt="" className={(props.img ? 'me-3': '')}/><strong>{props.text}</strong>
+    </Button>
   )
 }
 
