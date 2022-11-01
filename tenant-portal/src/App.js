fbf56b4a3c6cfbeb9e2e753916a5aaee78698b76
bhanu <bhanu.prakash@egovernments.org>
2022-11-01 18:15:54 +0530

    refactoring

--- tenant-portal/src/App.js
+++ tenant-portal/src/App.js
@@ -10,35 +10,35 @@ import config from "./config.json"
 import Footer from "./components/Footer/Footer";
 import ToastComponent from './components/Toast/Toast';
 import axios from 'axios';
-
-const {keycloak} = useKeycloak();
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response.status === 401) {
-      keycloak.logout()
-
-    }
-    else if (error.response.status === 403) {
-      <ToastComponent header="Unauthorized access" toastBody="You are not authorized to view this resource" />
-
-    }
-    else if(error.response){
-      <ToastComponent header="Error" toastBody={error.response.data} />
-    }
-  });
-
+import './i18n';
 
 function App() {
   const { initialized, keycloak } = useKeycloak();
+  axios.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response.status === 401) {
+        keycloak.logout()
+  
+      }
+      else if (error.response.status === 403) {
+        <ToastComponent header="Unauthorized access" toastBody="You are not authorized to view this resource" />
+  
+      }
+      else if(error.response){
+        <ToastComponent header="Error" toastBody={error.response.data} />
+      }
+    });
+ 
   if (!initialized) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
+       <div>
       <Router>
       <Header/>
+      <div style={{paddingBottom: "3rem", paddingTop: "3rem"}}>
         <Routes>
         
           <Route exact path={config.urlPath + "/"} element={<Home />} />
@@ -52,10 +52,13 @@ function App() {
            >
            </Route>
         </Routes>
+        </div>
+        <Footer/>
       </Router>
-      <Footer/>
+      
       <ToastComponent/>
     </div>
+   
   );
 }
 
