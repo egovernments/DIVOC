0db81f450aca44de107860b7bd9235127d187825
saiprakash-v <prakash@beehyv.com>
2022-11-02 09:46:50 +0530

    translations bug fixes

--- tenant-portal/src/App.js
+++ tenant-portal/src/App.js
@@ -11,27 +11,25 @@ import Footer from "./components/Footer/Footer";
 import ToastComponent from './components/Toast/Toast';
 import axios from 'axios';
 
-const {keycloak} = useKeycloak();
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response.status === 401) {
-      keycloak.logout() 
-     
-    }
-    else if (error.response.status === 403) {
-      <ToastComponent header="Unauthorized access" toastBody="You are not authorized to view this resource" />
-      
-    }
-    else if(error.response){
-      <ToastComponent header="Error" toastBody={error.response.data} /> 
-    }
-  });
-
-
 function App() {
   const { initialized, keycloak } = useKeycloak();
-  if (!initialized) {
+    axios.interceptors.response.use(
+        response => response,
+        error => {
+            if (error.response.status === 401) {
+                keycloak.logout()
+
+            }
+            else if (error.response.status === 403) {
+                <ToastComponent header="Unauthorized access" toastBody="You are not authorized to view this resource" />
+
+            }
+            else if(error.response){
+                <ToastComponent header="Error" toastBody={error.response.data} />
+            }
+        });
+
+    if (!initialized) {
     return <div>Loading...</div>;
   }
 
