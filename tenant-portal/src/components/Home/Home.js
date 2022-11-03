410c89fc6564a74c73507b51757e01651209cc1d
bhanu <bhanu.prakash@egovernments.org>
2022-11-03 13:26:48 +0530

    using Link instead of navigate

--- tenant-portal/src/components/Home/Home.js
+++ tenant-portal/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, Container, Col, Button, Row} from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import HomeImg from "../../assets/img/homeImage.png";
 import styles from './Home.module.css';
 import {useTranslation} from "react-i18next";
@@ -11,12 +11,6 @@ function Home() {
 
     const navigate = useNavigate();
     const { t } = useTranslation();
-    const genToken = () => {
-        navigate('/tenant-portal');
-    };
-    const manageSchema = () => {
-        navigate('/tenant-portal');
-    };
 
     useEffect(() => {
         navigate(config.urlPath + "/login");
@@ -33,20 +27,24 @@ function Home() {
                 <Container fluid>
                     <Row gutterX='3'>
                         <Col>
-                            <Card onClick={genToken} style={{ cursor: "pointer" }} className={styles['card']}>
+                        <Link to='/tenant-portal/generate-token' style={{textDecoration: 'none', }}>
+                            <Card style={{ cursor: "pointer" }} className={styles['card']}>
                                 <Card.Body>
                                     <Card.Title className={styles['card-title']}>{t('homePage.genTokenCard.title')}</Card.Title>
                                     <Card.Text className={styles['card-text']}>{t('homePage.genTokenCard.text')}</Card.Text>
                                 </Card.Body>
                             </Card> 
+                        </ Link>
                         </Col>
                         <Col>
-                            <Card onClick={manageSchema} style={{ cursor: "pointer" }}  className={styles['card']}>
+                        <Link to='/tenant-portal/manage-schema' style={{textDecoration: 'none', }}>
+                            <Card style={{ cursor: "pointer" }}  className={styles['card']}>
                                 <Card.Body>
                                     <Card.Title className={styles['card-title']}>{t('homePage.manageSchemaCard.title')}</Card.Title>
                                     <Card.Text className={styles['card-text']}>{t('homePage.manageSchemaCard.text')}</Card.Text>
                                 </Card.Body>
                             </Card> 
+                        </Link>
                         </Col>
                     </Row>
                 </Container>
