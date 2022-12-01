afb6090b57998ff12454a2576e701d4a43fd6772
ajay-beehyv <ajay.kumar@egovernments.org>
2022-12-01 20:02:30 +0530

    HLM-1099 | Create schema page

--- tenant-portal/src/components/CreateSchema/CreateSchema.js
+++ tenant-portal/src/components/CreateSchema/CreateSchema.js
@@ -1,7 +1,8 @@
 import React from "react";
 import axios from 'axios';
 import { useKeycloak } from "@react-keycloak/web";
-
+import upload_image from "../../assets/img/upload_image.png";
+import "./CreateSchema.css"
 function CreateSchema() {
     const { keycloak } = useKeycloak();
 
@@ -19,9 +20,30 @@ function CreateSchema() {
     }
 
     return(
-        <div>
-            <div>Create Schema</div>
+        <div  className="d-flex flex-column ">
+            <div className="d-flex justify-content-between" style={{padding: "2rem 8rem 1rem 8rem", maxHeight: "100%"}}>
+                <div className="col-6 px-5">
+                    <p className="title">Create New Schema</p>
+                    <form className="d-flex flex-column" id="create-schema">
+                        <div className="input-group">
+                            <label id="name" className="input-label">Name of the Schema</label>
+                            <input type="text" id="name" className="w-75 input-box"></input>
+                        </div>
+                        <div className="input-group"> 
+                            <label id="description" className="input-label">Description<span className="secondary-label">(optional)</span></label>
+                            <input type="text" id="description" className="w-75 input-box" style={{height: "100px"}}></input>
+                        </div>
+                    </form>
+                </div>
+                <div className="col-4 ">
+                    <img className="w-100" src={upload_image} ></img>
+                </div>
+            </div>
+            <div className="custom-footer justify-content-end">
+                <button className="btn-primary" type="submit" form="create-schema" >Save</button>
+            </div>
         </div>
+        
     );
 }
 
