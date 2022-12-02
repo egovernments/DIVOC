25200f0d9f3f85d8b62b32fd5c9d8a85ddd386f3
ajay-beehyv <ajay.kumar@egovernments.org>
2022-12-02 20:47:25 +0530

    HLM-1099 | Schema page and custom attributes

--- tenant-portal/src/components/CreateSchema/CreateSchema.js
+++ tenant-portal/src/components/CreateSchema/CreateSchema.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useKeycloak } from "@react-keycloak/web";
 import upload_image from "../../assets/img/upload_image.png";
 import "./CreateSchema.css"
+import { Button, Col, Container, Form, FormControl, FormGroup, Row, Image } from "react-bootstrap";
+
 function CreateSchema() {
     const { keycloak } = useKeycloak();
 
@@ -20,30 +22,32 @@ function CreateSchema() {
     }
 
     return(
-        <div  className="d-flex flex-column ">
-            <div className="d-flex justify-content-between" style={{padding: "2rem 8rem 1rem 8rem", maxHeight: "100%"}}>
-                <div className="col-6 px-5">
-                    <p className="title">Create New Schema</p>
-                    <form className="d-flex flex-column" id="create-schema">
-                        <div className="input-group">
-                            <label id="name" className="input-label">Name of the Schema</label>
-                            <input type="text" id="name" className="w-75 input-box"></input>
-                        </div>
-                        <div className="input-group"> 
-                            <label id="description" className="input-label">Description<span className="secondary-label">(optional)</span></label>
-                            <input type="text" id="description" className="w-75 input-box" style={{height: "100px"}}></input>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-4 ">
-                    <img className="w-100" src={upload_image} ></img>
-                </div>
-            </div>
-            <div className="custom-footer justify-content-end">
-                <button className="btn-primary" type="submit" form="create-schema" >Save</button>
-            </div>
+        <div>
+            <Container fluid="md" className="py-3">
+                <Row className="justify-content-between px-5" >
+                    <Col md={6}>
+                        <Row className="title gx-0">Create New Schema</Row>
+                        <Form id="create-schema" action="/manage-schema/create-schema/custom-attributes">
+                            <FormGroup className="py-3">
+                                <Form.Label className="input-label">Name of the Schema</Form.Label>
+                                <FormControl type="text" className="w-75 input-box"/>
+                            </FormGroup>
+                            <FormGroup className="py-3">
+                                <Form.Label className="input-label">Description<span className="secondary-label">(optional)</span></Form.Label>
+                                <FormControl type="text" className="w-75 input-box" style={{height: "100px"}}/>
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                    <Col md={5}>
+                        <Image className="w-100" src={upload_image} />
+                    </Col>
+                </Row>
+                
+            </Container>
+            <Row className="custom-footer justify-content-end w-100 gx-0 p-4">             
+                <Button type="submit" form="create-schema" >Save</Button>
+            </Row>
         </div>
-        
     );
 }
 
