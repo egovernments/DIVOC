79d8202fd48d7ae14e7684b0a76c23d0b474fed2
bhanu <bhanu.prakash@egovernments.org>
2022-12-06 13:36:02 +0530

    styles-specific to create schema

--- tenant-portal/src/components/CreateSchema/CreateSchema.js
+++ tenant-portal/src/components/CreateSchema/CreateSchema.js
@@ -1,5 +1,5 @@
 import React,{useState}  from "react";
-import "./CreateSchema.css";
+import styles from "./CreateSchema.module.css";
 import {useTranslation} from "react-i18next";
 import SchemaDetails from "../SchemaDetails/SchemaDetails"
 import upload_image from "../../assets/img/upload_image.png";
@@ -19,24 +19,24 @@ return (
             <Row className="justify-content-between px-5" >
                 <Col md={5} >
                     <Stack gap={4}>
-                        <Row className="title gx-0">{t('createSchema.title')}</Row>
+                        <Row className={styles["title"]}>{t('createSchema.title')}</Row>
                         <Row className="gx-0">{t('createSchema.text')}</Row>
                         <Col>{t('createSchema.view')} <Link to={''} className="text-decoration-none">{t('createSchema.trainingMaterial')}</Link> {t('createSchema.or')} <Link to={''} class="text-decoration-none">{t('createSchema.videosLink')}</Link></Col>
                         <Row xs={1} sm={2} className="pt-4">
                             <Col>
-                                <Card className="card" onClick={() => setSchemaDetails(true)}>
+                                <Card className={styles['card']} onClick={() => setSchemaDetails(true)}>
                                     <Card.Body className="">
                                         <Card.Title className="text-center"><Image src={addVector}/></Card.Title>
-                                        <Card.Text className="card-text">{t('createSchema.addText')}</Card.Text>
+                                        <Card.Text className={styles["card-text"]}>{t('createSchema.addText')}</Card.Text>
                                     </Card.Body>
                                 </Card> 
                             </Col>
                             <Col>
                                 <Link to={''} className="text-decoration-none">
-                                    <Card  className="card">
+                                    <Card  className={styles['card']}>
                                         <Card.Body className="">
                                             <Card.Title className="text-center"><Image src={uploadVector}/></Card.Title>
-                                            <Card.Text className="card-text">{t('createSchema.uploadText')}</Card.Text>
+                                            <Card.Text className={styles["card-text"]}>{t('createSchema.uploadText')}</Card.Text>
                                         </Card.Body>
                                     </Card> 
                                 </Link>
